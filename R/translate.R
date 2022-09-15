dd788263931759d0c635c4dfc07b5a0c78613424
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-09-15 15:53:07 +0200

    Add files via upload

--- R/translate.R
+++ R/translate.R
@@ -1,21 +1,25 @@
-#' translate() ##########################################################################
-#' can be used to translate values in a row by use of a dictionary
-#' similar purpose than merge, but better performance
-#' @param data       row in dataframe which has to be translated
-#' @param dictionary has to be a dataframe with rows "old" and "new" representing the link between the values to be replaced
-#' @example          translate(data$replaced,dictionary)
+translate <- function(data, dictionary, verbose = TRUE){
 
-translate <- function(data, dictionary, output=TRUE){
-  for (i in 1 : nrow(dictionary)){
-    if (i < nrow(dictionary) & output == TRUE){
-      base::cat(paste("\r", Sys.time(), "Remaining items to translate:", nrow(dictionary) - i, "                                          "))
+  loops <- nrow(dictionary)
+  nchar.loops <- nchar(loops)
+  colnames.dictionary <- colnames(dictionary)
+
+  if (verbose == FALSE){
+    for (i in 1 : loops){
+      data[data == dictionary[i, grep("old", colnames.dictionary)]] <- dictionary[i, grep("new", colnames.dictionary)]
     }
-
-    data[data == dictionary[i, grep("old", colnames(dictionary))]] <- dictionary[i, grep("new", colnames(dictionary))]
-
-    if (i == nrow(dictionary) & output == TRUE){
-      base::cat(paste("\r", Sys.time(), nrow(dictionary), "items translated                                              "))
-    }
+  }
+  if (verbose == TRUE){
+    for (i in 1 : loops){
+      Sys.sleep(0.2)
+      base::cat(sprintf(paste0("\r %s: remaining items to translate: % ", nchar.loops, "s"),
+                  Sys.time(), loops - i))
+      data[data == dictionary[i, grep("old", colnames.dictionary)]] <- dictionary[i, grep("new", colnames.dictionary)]
+      }
+  }
+  if (verbose == TRUE){
+    base::cat(sprintf(paste0("\r %s: %", nchar.loops+13, "s items translated\n"),
+                Sys.time(), loops))
   }
   return(data)
 }
