1dce0294b9ce3a34f864813e546360609ec6a685
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-09-16 21:49:51 +0200

    Update mypackageinstall.R

--- R/mypackageinstall.R
+++ R/mypackageinstall.R
@@ -7,7 +7,7 @@ mypackage.install <- function() {
   if (any(to_install)){
     cat(paste0("\n", Sys.time(), ": install missing packages '", paste(packages[to_install], collapse=", "), "'\n"))
     install.packages(packages[to_install], dependencies = T)
-    cat(paste0("\n", Sys.time(), ": missing packages '", paste(packages, collapse=", "), "' installed\n\n"))
+    cat(paste0("\n", Sys.time(), ": missing packages '", paste(packages[to_install], collapse=", "), "' installed\n\n"))
   }
   else{
     cat(paste0("\n", Sys.time(), ": all packages installed\n\n"))
