03b1fd7141c83781df755f3ecb62583e16b1a069
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-09-16 21:43:50 +0200

    Update mypackageinstall.R

--- R/mypackageinstall.R
+++ R/mypackageinstall.R
@@ -1,8 +1,16 @@
 packages <- c("feather", "raster", "terra", "caret", "tidyverse", "ranger", "randomForest", "foreach", "ggplot2",
               "rgdal", "e1071", "s2", "sf", "lidR", "RMariaDB", "remotes", "memuse", "gdata")
 
-to_install <- !packages %in% installed.packages()
-if (any(to_install)){
-  cat("\n\n\nPackages missing:",paste0(packages[to_install],sep=""),"\n")
-  cat("use 'mypackageinstall()' to add\n\n\n")
+package.install <- function(packages) {
+  to_install <- !packages %in% installed.packages()
+  if (any(to_install)){
+    cat(paste0("\n", Sys.time(), ": install missing packages '", paste(packages, collapse=", "), "'\n"))
+    install.packages(packages[to_install], dependencies = T)
+    cat(paste0("\n", Sys.time(), ": missing packages '", paste(packages, collapse=", "), "' installed\n\n"))
+  }
+  else{
+    cat(paste0("\n", Sys.time(), ": all packages installed\n\n"))
+  }
 }
+
+package.install(packages)
