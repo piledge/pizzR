22ed6e9ca2bed9518a5bc024f0f01013f75a6052
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-09-16 22:08:35 +0200

    Update checkpackages.R

--- R/checkpackages.R
+++ R/checkpackages.R
@@ -1,5 +1,5 @@
-packages <- c("feather", "raster", "terra", "caret", "tidyverse", "ranger", "randomForest", "foreach", "ggplot2",
-              "rgdal", "e1071", "s2", "sf", "lidR", "RMariaDB", "remotes", "memuse", "gdata")
+packages <- c("caret", "e1071", "feather", "foreach", "gdata", "ggplot2", "lidR", "memuse", "randomForest", "ranger",
+              "raster", "remotes", "rgdal", "RMariaDB", "s2", "sf", "terra", "tidyverse")
 
 to_install <- !packages %in% installed.packages()
 if (any(to_install)){
