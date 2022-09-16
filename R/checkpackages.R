877899557af591cd422e6a362bc247b28ba22932
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-09-16 21:49:05 +0200

    Update checkpackages.R

--- R/checkpackages.R
+++ R/checkpackages.R
@@ -4,5 +4,5 @@ packages <- c("feather", "raster", "terra", "caret", "tidyverse", "ranger", "ran
 to_install <- !packages %in% installed.packages()
 if (any(to_install)){
   cat("\n\n\nPackages missing:",paste0(packages[to_install],sep=""),"\n")
-  cat("use 'mypackage.install()' to add\n\n\n")
+  cat("use 'pizzR::mypackage.install()' to add\n\n\n")
 }
