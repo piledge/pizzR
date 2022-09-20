20187dc3c9531059c7f38c3080dcd3eaa49fcb21
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-09-20 14:00:44 +0200

    Add files via upload

---
+++ R/serversuspend.R
@@ -0,0 +1,9 @@
+server.suspend <- function(x=60,y='sh /media/geodata1/suspend.sh'){
+  
+  for (i in 1:x){
+    cat(paste0("\r",Sys.time(), ": node '",Sys.info()[4],"' will suspend in ", x - i + 1, " seconds!   "))
+    Sys.sleep(1)
+  }
+  cat(paste0("\r",Sys.time(), ": planned suspension after processing   "))
+  system(y)
+}
