5ff4acc2208698930f52683ac3090bb4edbba68c
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-09-29 17:54:44 +0200

    Update translate.R

--- R/translate.R
+++ R/translate.R
@@ -1,5 +1,5 @@
 translate <- function(x,y,verbose=FALSE){
-  if (length(grep("new",colnames(dictionary))) != 1 | length(grep("old",colnames(dictionary))) != 1) return(warning("No suitable dictionary!\n"))
+  if (length(grep("new",colnames(y))) != 1 | length(grep("old",colnames(y))) != 1) return(warning("No suitable dictionary!\n"))
 
   loops <- nrow(y)
   nchar.loops <- nchar(loops)
@@ -7,7 +7,7 @@ translate <- function(x,y,verbose=FALSE){
 
   if (verbose == FALSE){
     for (i in 1 : loops){
-      x[x == dictionary[i, grep("old", colnames_y)]] <- y[i, grep("new", colnames_y)]
+      x[x == y[i, grep("old", colnames_y)]] <- y[i, grep("new", colnames_y)]
     }
   }
   if (verbose == TRUE){
