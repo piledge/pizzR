2fdae4579aaef15a1a0a478f39adc1d36486f4e0
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-11-08 15:38:24 +0100

    Update optdatatype.R

--- R/optdatatype.R
+++ R/optdatatype.R
@@ -1,46 +1,40 @@
-opt.datatype <- function(spatialraster){
-
-  sample.size <- function(cellnumber, k1 = 0.5, d1 = 5000, k2 = 0.25, d2 = 10000, maxsample = 15000){
-    if (k2 * (cellnumber - d2) + k1*(d2-d1)+d1 > maxsample){
-      return(maxsample)
-    }
-    if (cellnumber > d2){
-      return(k2 * (cellnumber - d2) + k1*(d2-d1)+d1)
-    }else if (cellnumber > d1){
-      return(k1 * (cellnumber - d1) + d1)
-    }else{
-      return(cellnumber)
-    }
-  }
-
+opt.datatype <- function(spatialraster, samplesize=25){
   rsttype <- class(spatialraster)[1]
-
-  if(rsttype == "SpatRaster") rst_sample_cells <- terra::spatSample(spatialraster, sample.size(ncell(spatialraster)), "random", values = TRUE, na.rm = TRUE, warn = FALSE)
-  if(rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack") rst_sample_cells <- raster::sampleRandom(spatialraster, sample.size(ncell(spatialraster)), na.rm = TRUE)
-  if(rsttype != "SpatRaster" && rsttype != "RasterLayer" && rsttype != "RasterBrick" && rsttype != "RasterStack") return(warning("Not a suitable rasterfile!\n"))
-
+  
+  if (samplesize <= 0) return(warning("Samplesize must be >0!\n"))
+  if (ncell(spatialraster) < samplesize) size <- ncell(spatialraster) else size <- samplesize
+  
+  if (rsttype == "SpatRaster") rst_sample_cells <- terra::spatSample(spatialraster, size, "random", values = TRUE, na.rm = TRUE, warn = FALSE)
+  if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack") rst_sample_cells <- as.data.frame(raster::sampleRandom(spatialraster, size, na.rm = TRUE))
+  if (rsttype != "SpatRaster" && rsttype != "RasterLayer" && rsttype != "RasterBrick" && rsttype != "RasterStack") return(warning("Not a suitable rasterfile!\n"))
+  
   rst_sample_cells[rst_sample_cells==Inf] <- NA
-
-  rst_min <- range(rst_sample_cells)[1]
-  rst_max <- range(rst_sample_cells)[2]
-
+  
+  if (rsttype == "SpatRaster"){
+    rst_min <- min(spatialraster@ptr[["range_min"]])
+    rst_max <- max(spatialraster@ptr[["range_max"]])
+  }
+  if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack"){
+    rst_min <- min(minValue(spatialraster))
+    rst_max <- max(maxValue(spatialraster))
+  }
+  
   rst_significant_value <- max(abs(c(rst_min, rst_max)))
   rst_signed <- rst_min < 0
-
+  
   rst_float <- TRUE
-  if(all((floor(rst_sample_cells) / rst_sample_cells) == 1, na.rm = T)) rst_float <- FALSE
-
-
+  if (all((floor(rst_sample_cells) / rst_sample_cells) == 1, na.rm = T)) rst_float <- FALSE
+  
   if (nrow(rst_sample_cells) == 0) return("LOG1S")                                #LOG
   if (is.logical(rst_sample_cells[,1:ncol(rst_sample_cells)])) return("LOG1S")
-
+  
   if (rst_float == T){                            #FLOAT
     if (rst_significant_value < 3.4e+38){
       return("FLT4S")
-  }else
+    }else
       return("FLT8S")
   }
-
+  
   if (rst_float == FALSE){                            #INT
     if (rst_signed == TRUE){                             #INTS
       if (rst_significant_value <= 127){
