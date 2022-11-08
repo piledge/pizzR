3ee3b2a2d49952e1d7c74d1c25ecbde5df96379c
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-11-08 16:06:26 +0100

    Update writeslimraster.R

--- R/writeslimraster.R
+++ R/writeslimraster.R
@@ -1,24 +1,24 @@
 writeslimRaster <- function(x, filename, compression=T, overwrite=T, BIGTIFF="IF_NEEDED", filetype="GTiff", samplesize=25, ...){
   
-  opt.datatype <- function(spatialraster, samplesize=samplesize){
-    rsttype <- class(spatialraster)[1]
+  opt.datatype <- function(x, x.samplesize=samplesize){
+    rsttype <- class(x)[1]
     
-    if (samplesize <= 0) return(warning("Samplesize must be >0!\n"))
-    if (ncell(spatialraster) < samplesize) size <- ncell(spatialraster) else size <- samplesize
+    if (x.samplesize <= 0) return(warning("x.samplesize must be >0!\n"))
+    if (ncell(x) < x.samplesize) size <- ncell(x) else size <- x.samplesize
     
-    if (rsttype == "SpatRaster") rst_sample_cells <- terra::spatSample(spatialraster, size, "random", values = TRUE, na.rm = TRUE, warn = FALSE)
-    if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack") rst_sample_cells <- as.data.frame(raster::sampleRandom(spatialraster, size, na.rm = TRUE))
+    if (rsttype == "SpatRaster") rst_sample_cells <- terra::spatSample(x, size, "random", values = TRUE, na.rm = TRUE, warn = FALSE)
+    if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack") rst_sample_cells <- as.data.frame(raster::sampleRandom(x, size, na.rm = TRUE))
     if (rsttype != "SpatRaster" && rsttype != "RasterLayer" && rsttype != "RasterBrick" && rsttype != "RasterStack") return(warning("Not a suitable rasterfile!\n"))
     
     rst_sample_cells[rst_sample_cells==Inf] <- NA
     
     if (rsttype == "SpatRaster"){
-      rst_min <- min(spatialraster@ptr[["range_min"]])
-      rst_max <- max(spatialraster@ptr[["range_max"]])
+      rst_min <- min(x@ptr[["range_min"]])
+      rst_max <- max(x@ptr[["range_max"]])
     }
     if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack"){
-      rst_min <- min(minValue(spatialraster))
-      rst_max <- max(maxValue(spatialraster))
+      rst_min <- min(minValue(x))
+      rst_max <- max(maxValue(x))
     }
     
     rst_significant_value <- max(abs(c(rst_min, rst_max)))
@@ -27,18 +27,18 @@ writeslimRaster <- function(x, filename, compression=T, overwrite=T, BIGTIFF="IF
     rst_float <- TRUE
     if (all((floor(rst_sample_cells) / rst_sample_cells) == 1, na.rm = T)) rst_float <- FALSE
     
-    if (nrow(rst_sample_cells) == 0) return("LOG1S")                                #LOG
+    if (nrow(rst_sample_cells) == 0) return("LOG1S")                              #LOG
     if (is.logical(rst_sample_cells[,1:ncol(rst_sample_cells)])) return("LOG1S")
     
-    if (rst_float == T){                               #FLOAT
+    if (rst_float == T){                                                          #FLOAT
       if (rst_significant_value < 3.4e+38){
         return("FLT4S")
       }else
         return("FLT8S")
     }
     
-    if (rst_float == FALSE){                           #INT
-      if (rst_signed == TRUE){                         #INTS
+    if (rst_float == FALSE){                                                      #INT
+      if (rst_signed == TRUE){                                                    #INTS
         if (rst_significant_value <= 127){
           return("INT2S")
         }
@@ -49,7 +49,7 @@ writeslimRaster <- function(x, filename, compression=T, overwrite=T, BIGTIFF="IF
           return("INT4S")
         }
       }
-      if (rst_signed == FALSE){                        #INTU
+      if (rst_signed == FALSE){                                                   #INTU
         if (rst_significant_value <= 254){
           return("INT2U")
         }
