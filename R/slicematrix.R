82f5ba7ed2fde4f32150c9f8f9a3f2690a4cf8c7
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-12-19 15:25:05 +0100

    Update slicematrix.R

--- R/slicematrix.R
+++ R/slicematrix.R
@@ -1,13 +1,14 @@
-slice.matrix <- function(x, nslices=1, windowsize=1, slice='h'){
+slice.matrix <- function(x, nslices=1, windowsize=1, direction='h'){
   
-  if (slice == 'h') base <- ncol(x)
-  if (slice == 'v') base <- nrow(x)
-  if (slice != 'h' && slice != 'v') return(warning("slice can only be 'h' or 'v'!"))
+  if (direction == 'h') base <- nrow(x)
+  if (direction == 'v') base <- ncol(x)
+  if (direction != 'h' && direction != 'v') return(warning("slice can only be 'h' or 'v'!"))
   
   normal.slice <- floor(base / (nslices))
   last.slice <- base - (nslices * normal.slice) + normal.slice
   if (nslices == 1) last.slice <- 0
-  slice.size <- c(rep(normal.slice, nslices - 1), last.slice)
+  if ((base / (nslices * normal.slice)) == 1) last.slice <- 0
+  #slice.size <- c(rep(normal.slice, nslices - 1), last.slice)
   
   oversize <- floor(windowsize / 2)
   
@@ -16,8 +17,9 @@ slice.matrix <- function(x, nslices=1, windowsize=1, slice='h'){
     selections[[1]] <- 1:base
   }else{
     selections[[1]] <- 1:(normal.slice[1] + oversize)
-    for (i in 1:(nslices - 2)) selections[[i+1]] <- (i * (normal.slice + 1) - oversize):((i+1) * normal.slice + oversize)
-    selections[[nslices]] <- (base - last.slice - oversize + 1):base
+    for (i in 1:(nslices - 2)) selections[[i+1]] <- (i * (normal.slice) - oversize):((i+1) * normal.slice + oversize)
+    if (last.slice != 0)  selections[[nslices]] <- (base - last.slice - oversize):base
+    if (last.slice == 0)  selections[[nslices]] <- (base - normal.slice - oversize):base
   }
   return(selections)
 }
