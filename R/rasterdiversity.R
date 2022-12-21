4345fd5ea08a32d472f5b31292d5c9a795a785c2
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2022-12-21 12:02:44 +0100

    Add files via upload

---
+++ R/rasterdiversity.R
@@ -0,0 +1,45 @@
+rasterdiversity <- function(x, index='shannon', w=3){
+  
+  if (index != 'eveness' && index != 'raosQ' && index != 'richness' && index != 'shannon' && index != 'simpson') return(cat("Index can either be 'eveness', 'raosQ', 'richness', 'shannon' or 'simpson'"))
+  
+  if (index == 'eveness'){
+    fun <- function(x) {
+      cnts <- table(x)
+      cnts <- cnts / sum(cnts)
+      shan <- -sum(cnts * log(cnts))
+      even <- shan / max(shan)
+    }
+  }
+  
+  if (index == 'raosQ'){
+    div <- length(x)^2
+    fun <- function(x, distance = 'euclidean') sum(as.matrix(dist(x, method = distance)) / div)
+  }
+  
+  if (index == 'richness'){
+    fun <- function(x) length(unique(na.omit(x)))
+  }
+  
+  if (index == 'shannon'){
+    fun <- function(x) {
+      cnts <- table(x)
+      cnts <- cnts / sum(cnts)
+      -sum(cnts * log(cnts))
+    }
+  }
+  
+  if (index == 'simpson'){
+    fun <- function(x) {
+      cnts <- table(x)
+      cnts <- cnts / sum(cnts)
+      -log(sum(cnts^2))
+    }
+  }
+  
+  fparameters             <- list()
+  fparameters$x           <- x
+  fparameters$w           <- w
+  fparameters$fun         <- fun
+  
+  return(do.call(terra::focal, fparameters))
+}
