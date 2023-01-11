98d73efa642bacb5f8c6de734d33536d23690e8a
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-01-11 12:20:05 +0100

    Update changerasteroptions.R

--- R/changerasteroptions.R
+++ R/changerasteroptions.R
@@ -13,7 +13,7 @@ change.rasterOptions <- function(changetmpdir=FALSE, tmpdir="", OSRAM.remaining=
 
   require(raster)
 
-  if (isTRUE(changetmpdir) && dir.exists(tmpdir) == FALSE) {
+  if (changetmpdir && dir.exists(tmpdir) == FALSE) {
     base::dir.create(tmpdir, recursive = TRUE)
     base::cat(paste("\n", Sys.time(), tmpdir, "created"))
   }
@@ -26,11 +26,11 @@ change.rasterOptions <- function(changetmpdir=FALSE, tmpdir="", OSRAM.remaining=
   }
 
                          fparameters                  <- list(...)
-  if (changetmpdir == T) fparameters$tmpdir           <- tmpdir
+  if (changetmpdir) fparameters$tmpdir           <- tmpdir
                          fparameters$memfrac          <- memfrac
                          fparameters$maxmemory        <- maxmemory * 1024^3
                          fparameters$progress         <- progress
 
   do.call(raster::rasterOptions, fparameters)
-  if (isTRUE(verbose)) raster::rasterOptions()
+  if (verbose) raster::rasterOptions()
 }
