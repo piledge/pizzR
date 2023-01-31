e717f5710710368e991023c1c78123febc05cca0
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-01-31 11:22:16 +0100

    Update translate.R

--- R/translate.R
+++ R/translate.R
@@ -1,24 +1,27 @@
-translate <- function(x,y,verbose=FALSE){
+translate <- function(x,y,verbose=F){
+
   if (length(grep("new",colnames(y))) != 1 | length(grep("old",colnames(y))) != 1) return(warning("No suitable dictionary!\n"))
+  if (!is.logical(verbose)) return(warning("'verbose' has to be of class logical!\n"))
 
-  loops <- nrow(y)
-  nchar.loops <- nchar(loops)
+  loops <- seq_len(nrow(y))
+  nchar.loops <- nchar(max(loops))
   colnames_y <- colnames(y)
+  colnr_old <- grep("old", colnames_y)
+  colnr_new <- grep("new", colnames_y)
 
-  if (verbose == FALSE){
-    for (i in 1 : loops){
-      x[x == y[i, grep("old", colnames_y)]] <- y[i, grep("new", colnames_y)]
+  if (!verbose){
+    for (i in loops){
+      x[x == y[i, colnr_new]] <- y[i, colnr_new]
     }
   }
-  if (verbose == TRUE){
-    for (i in 1 : loops){
-      Sys.sleep(0.2)
+  if (verbose){
+    for (i in loops){
       base::cat(sprintf(paste0("\r %s: remaining items to translate: % ", nchar.loops, "s"),
                   Sys.time(), loops - i))
-      x[x == y[i, grep("old", colnames_y)]] <- y[i, grep("new", colnames_y)]
+      x[x == y[i, colnr_new]] <- y[i, colnr_new]
       }
   }
-  if (verbose == TRUE){
+  if (verbose){
     base::cat(sprintf(paste0("\r %s: %", nchar.loops+13, "s items translated\n"),
                 Sys.time(), loops))
   }
