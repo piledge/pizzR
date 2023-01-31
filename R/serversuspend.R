1db4abbd8beb110b365a481a495a87f92de6b815
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-01-31 14:02:45 +0100

    Update serversuspend.R

--- R/serversuspend.R
+++ R/serversuspend.R
@@ -1,6 +1,6 @@
 server.suspend <- function(x=60, y='sh /media/geodata1/suspend.sh'){
   
-  for (i in 1:x){
+  for (i in seq_len(x)){
     cat(paste0("\r",Sys.time(), ": node '",Sys.info()[4],"' will suspend in ", x - i + 1, " seconds!   "))
     Sys.sleep(1)
   }
