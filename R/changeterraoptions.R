d6b3c8be476a93a9394131535c57a439f393c357
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-01-31 10:32:33 +0100

    Update changeterraoptions.R

--- R/changeterraoptions.R
+++ R/changeterraoptions.R
@@ -1,5 +1,8 @@
-change.terraOptions <- function(changetempdir=FALSE, tempdir="", OSRAM.remaining=3, progress=0, verbose=FALSE, ...){
-
+change.terraOptions <- function(changetempdir=F, tempdir="", OSRAM.remaining=3, progress=0, verbose=F, ...){
+  
+  if (!is.logical(changetempdir)) return(warning("'changetempdir' has to be of class logical!\n"))
+  if (!is.logical(verbose)) return(warning("'verbose' has to be of class logical!\n"))
+  
   package.install <- function(x) {
     to_install <- !x %in% installed.packages()
     if (any(to_install)){
@@ -10,27 +13,26 @@ change.terraOptions <- function(changetempdir=FALSE, tempdir="", OSRAM.remaining
   }
   package.install(c("memuse", "raster", "Rcpp", "terra"))
 
-
   require(terra)
 
-  if (isTRUE(changetempdir) && dir.exists(tempdir) == FALSE) {
-    base::dir.create(tempdir, recursive = TRUE)
+  if (changetempdir && dir.exists(tempdir) == F) {
+    base::dir.create(tempdir, recursive = T)
     base::cat(paste("\n", Sys.time(), tempdir, "created"))
   }
-
+  
   memmax <- memuse::Sys.meminfo()$totalram@size
   if (((memmax-OSRAM.remaining) / memmax) > 0.9){
     memfrac  <-  0.9
   }else{
     memfrac <- ((memmax - OSRAM.remaining) / memmax)
   }
-
-                          fparameters                  <- list(...)
-  if (changetempdir == T) fparameters$tempdir          <- tempdir
-                          fparameters$memfrac          <- memfrac
-                          fparameters$memmax           <- memmax
-                          fparameters$progress         <- progress
-
+  
+  fparameters                  <- list(...)
+  if (changetempdir) fparameters$tempdir          <- tempdir
+  fparameters$memfrac          <- memfrac
+  fparameters$memmax           <- memmax
+  fparameters$progress         <- progress
+  
   do.call(terra::terraOptions, fparameters)
-  if (isTRUE(verbose)) terra::terraOptions()
+  if (verbose) terra::terraOptions()
 }
