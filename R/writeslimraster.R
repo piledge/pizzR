de5fabb6e13c18fc11ddb15da0d193f85fbc787a
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-02-02 13:34:50 +0100

    Update writeslimraster.R

--- R/writeslimraster.R
+++ R/writeslimraster.R
@@ -1,45 +1,44 @@
-writeslimRaster <- function(x, filename, compression=T, overwrite=T, BIGTIFF="YES", filetype="GTiff", samplesize=100, datatype=NULL, NAflag=NULL, ...){
-  
+writeslimRaster <- function(x, filename, compression=T, overwrite=T, BIGTIFF="YES", filetype="GTiff", samplesize=25, datatype="ESTIMATE", ...){
+
   rsttype <- class(x)[1]
   if (rsttype != "SpatRaster" && rsttype != "RasterLayer" && rsttype != "RasterBrick" && rsttype != "RasterStack") return(warning("Not a suitable rasterfile!\n"))
   if (!is.logical(compression)) return(warning("'compression' needs to be boolean!\n"))
   if (!is.logical(overwrite)) return(warning("'overwrite' needs to be boolean!\n"))
   if (BIGTIFF != "YES" && BIGTIFF != "NO" && BIGTIFF != "IF_SAFER" && BIGTIFF != "IF_NEEDED") return(warning("'BIGTIFF' should be one of the following options: YES/NO/IF_SAFER/IF_NEEDED!\n"))
-  if (datatype != "LOG1S" && datatype != "INT1S" && datatype != "INT1U" && datatype != "INT2S" && datatype != "INT2U" && datatype != "INT4S" && datatype != "INT4U" && datatype != "FLT4S" && datatype != "FLT8S" && !is.null(datatype)) return(warning("'datatype' should be either ESTIMATE or one of the following options: LOG1S/INT1S/INT1U/INT2S/INT2U/INT4S/INT4U/FLT4S/FLT8S!\n"))
+  if (datatype != "LOG1S" && datatype != "INT1S" && datatype != "INT1U" && datatype != "INT2S" && datatype != "INT2U" && datatype != "INT4S" && datatype != "INT4U" && datatype != "FLT4S" && datatype != "FLT8S" && datatype != "ESTIMATE") return(warning("'datatype' should be either ESTIMATE or one of the following options: LOG1S/INT1S/INT1U/INT2S/INT2U/INT4S/INT4U/FLT4S/FLT8S!\n"))
   
-  fparameters                                     <- list(...)
-  fparameters$x                                   <- x
-  fparameters$filename                            <- filename
-  fparameters$overwrite                           <- overwrite
-  if (!is.null(datatype)) fparameters$datatype    <- datatype
-  if (!is.null(NAflag))   fparameters$NAflag      <- NAflag
+  fparameters             <- list(...)
+  fparameters$x           <- x
+  fparameters$filename    <- filename
+  fparameters$overwrite   <- overwrite
   
-  if (is.null(datatype) || (is.null(NAflag))){
-    cat(paste0("\n", Sys.time(), ": Estimate raster attributes ..."))
-    get.attributes                                                     <- pizzR::opt.datatype(x, samplesize)
-    if (is.null(datatype))                        fparameters$datatype <- get.attributes$datatype
-    if (is.null(NAflag) && isFALSE(NAflag))       fparameters$NAflag   <- NA
-  }
-
   if (rsttype == "SpatRaster"){
     
-    fparameters$filetype                <- filetype
-    if (!compression) fparameters$gdal  <- c(paste0("BIGTIFF = ", BIGTIFF))
-    if (compression)  fparameters$gdal  <- c(paste0("BIGTIFF = ", BIGTIFF), "COMPRESS = DEFLATE", "ZLEVEL = 9", "PREDICTOR = 2")
-
-    if (compression)  cat(paste0("\n", Sys.time(), ": Write slim rasterfile as '", fparameters$datatype,"' ...\n"))
-    if (!compression) cat(paste0("\n", Sys.time(), ": Write rasterfile as '", fparameters$datatype,"' ...\n"))
+    fparameters$filetype                                                        <- filetype
+    if (compression == TRUE && datatype == "ESTIMATE"){
+      cat(paste0("\n", Sys.time(), ": Estimate datatype ..."))
+      fparameters$datatype                                                      <- pizzR::opt.datatype(x, samplesize)
+    }
+    if (compression == TRUE)  fparameters$gdal                                  <- c(paste0("BIGTIFF = ", BIGTIFF), "COMPRESS = DEFLATE", "ZLEVEL = 9", "PREDICTOR = 2")
+    if (compression == FALSE) fparameters$gdal                                  <- c(paste0("BIGTIFF = ", BIGTIFF))
+    
+    if (compression == TRUE)  cat(paste0("\n", Sys.time(), ": Write slim rasterfile as '", fparameters$datatype,"' ...\n"))
+    if (compression == FALSE) cat(paste0("\n", Sys.time(), ": Write rasterfile ...\n"))
     do.call(terra::writeRaster, fparameters)
   }
   
   if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack"){
     
-    fparameters$format                      <- filetype
-    fparameters$BIGTIFF                     <- BIGTIFF
-    if (compression)  fparameters$options   <- c("COMPRESS=DEFLATE", "PREDICTOR=2", "ZLEVEL=9")
-
-    if (compression)  cat(paste0("\n", Sys.time(), ": Write slim rasterfile as '", fparameters$datatype,"' ...\n"))
-    if (!compression) cat(paste0("\n", Sys.time(), ": Write rasterfile as '", fparameters$datatype,"' ...\n"))
+    fparameters$format                                                          <- filetype
+    if (compression == TRUE && datatype == "ESTIMATE"){
+      cat(paste0("\n", Sys.time(), ": Estimate datatype ..."))
+      fparameters$datatype                                                      <- pizzR::opt.datatype(x, samplesize)
+    }
+    if (compression == TRUE && datatype != "ESTIMATE") fparameters$datatype     <- datatype
+    if (compression == TRUE) fparameters$options                                <- c("COMPRESS=DEFLATE", "PREDICTOR=2", "ZLEVEL=9")
+    
+    if (compression == TRUE)  cat(paste0("\n", Sys.time(), ": Write slim rasterfile as '", fparameters$datatype,"' ...\n"))
+    if (compression == FALSE) cat(paste0("\n", Sys.time(), ": Write rasterfile ...\n"))
     do.call(raster::writeRaster, fparameters)
   }
   cat(paste0("\n", Sys.time(), ": Done ...\n"))
