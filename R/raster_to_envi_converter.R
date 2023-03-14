780ee6db9c672f82c24dd92f0737c555770afedd
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-03-14 14:20:33 +0100

    Add files via upload

---
+++ R/raster_to_envi_converter.R
@@ -0,0 +1,17 @@
+raster_to_envi_converter <- function(input.path, output.path, interleave = 'BSQ', input.filetype = '.tif$'){
+  
+  interleave <- toupper(interleave)
+  if (!interleave %in% c('BSQ', 'BIL', 'BIP'))  return(warning("'interleave' has to be 'BSQ', 'BIL' or 'BIP'"))
+    rfiles <- list.files(input.path, full.names = T, pattern = input.filetype)
+  if (length(rfiles) == 0)  return(warning("No raster files found!"))
+    
+  
+  pizzR::setcreate.wd(output.path)
+  for (i in seq(rfiles)) terra::writeRaster(terra::rast(rfiles[i]), overwrite = T,
+                                            filename = sub('.tif', '', basename(rfiles[i])), filetype="ENVI",
+                                            gdal=c(paste0('INTERLEAVE=', interleave), "BIGTIFF = IF_NEEDED", "COMPRESS = DEFLATE", "ZLEVEL = 9", ""))
+}
+
+
+pizzR::raster_to_envi_converter('D:/S2 Jahre/BPWW_2022/ok', 'D:/ENVI_BSQ', 'BSQ', '.tif$')
+
