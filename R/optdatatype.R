ac21d0ffb599ee608cbdb54818eddbb7c6f738e1
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-10-23 13:48:46 +0200

    Update optdatatype.R
    faster implemenation

--- R/optdatatype.R
+++ R/optdatatype.R
@@ -1,94 +1,60 @@
-opt.datatype <- function(x, samplesize=25){
-
-    package.install <- function(x) {
+opt.datatype <- function(x) {
+  package.install <- function(x) {
     to_install <- !x %in% installed.packages()
-    if (any(to_install)){
-      cat(paste0(Sys.time(), ": install missing packages '", paste(x[to_install], collapse=", "), "'\n"))
+    if (any(to_install)) {
+      cat(paste0(Sys.time(), ": install missing packages '", 
+                 paste(x[to_install], collapse = ", "), "'\n"))
       install.packages(x[to_install], dependencies = T)
-      cat(paste0(Sys.time(), ": missing packages '", paste(x[to_install], collapse=", "), "' installed\n\n"))
+      cat(paste0(Sys.time(), ": missing packages '", paste(x[to_install], 
+                                                           collapse = ", "), "' installed\n\n"))
     }
   }
   package.install(c("memuse", "raster", "Rcpp", "terra"))
-  
   rsttype <- class(x)[1]
+
+  if (rsttype != "SpatRaster" && rsttype != "RasterLayer" && 
+      rsttype != "RasterBrick" && rsttype != "RasterStack") 
+    return(warning("Not a suitable rasterfile!\n"))
+
+  if (rsttype == "SpatRaster")                                                            minmaxvals <- terra::minmax(rst.terra)
+  if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack")   minmaxvals <- raster::minValue(rst.raster)
   
-  if (samplesize <= 0) return(warning("Samplesize must be >0!\n"))
-  if (terra::ncell(x) < samplesize) size <- terra::ncell(x) else size <- samplesize
-  
-  if (rsttype == "SpatRaster") rst_sample_cells <- terra::spatSample(x, size, "random", values = TRUE, na.rm = TRUE, warn = FALSE)
-  if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack") rst_sample_cells <- as.data.frame(raster::sampleRandom(x, size, na.rm = TRUE))
-  if (rsttype != "SpatRaster" && rsttype != "RasterLayer" && rsttype != "RasterBrick" && rsttype != "RasterStack") return(warning("Not a suitable rasterfile!\n"))
-  
-  rst_sample_cells[rst_sample_cells==Inf] <- NA
-  
-  if (rsttype == "SpatRaster"){         
-    rst_min <- min(x@ptr[["range_min"]])
-    rst_max <- max(x@ptr[["range_max"]])
-    
-    if (is.na(rst_min) || is.na(rst_max)){                                        #slower; if ptr not available
-      rst.summary <- terra::summary(x, warn = F)
-      
-      rst_min <- rst.summary[1,]
-      rst_min <- unlist(strsplit(rst_min, ":"))
-      rst_min <- rst_min[-grep("Min", rst_min)]
-      rst_min <- min(as.numeric(rst_min))
-      
-      rst_max <- rst.summary[6,]
-      rst_max <- unlist(strsplit(rst_max, ":"))
-      rst_max <- rst_max[-grep("Max", rst_max)]
-      rst_max <- min(as.numeric(rst_max))
-    }
-  }
-  if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack"){
-    rst_min <- min(minValue(x))
-    rst_max <- max(maxValue(x))
-    
-    if (is.na(rst_min) || is.na(rst_max)){                                        #slower; if ptr not available
-      rst.summary <- raster::summary(x, maxsamp = 10000)
-      min <- rst.summary[1,]
-      max <- rst.summary[1,]
-      
-      min(rst.summary[rownames(rst.summary) == "Min.", ])
-      max(rst.summary[rownames(rst.summary) == "Max.", ])
-    }
-  }
-  
+  rst_min <- min(minmaxvals)
+  rst_max <- max(minmaxvals)
   rst_significant_value <- max(abs(c(rst_min, rst_max)))
   rst_signed <- rst_min < 0
-  
   rst_float <- TRUE
-  if (all((floor(rst_sample_cells) / rst_sample_cells) == 1, na.rm = T)) rst_float <- FALSE
   
-  if (nrow(rst_sample_cells) == 0) return("LOG1S")                              #LOG
-  if (is.logical(rst_sample_cells[,1:ncol(rst_sample_cells)])) return("LOG1S")
+  if(all((floor(minmaxvals)/minmaxvals) == 1, na.rm = T)) rst_float <- FALSE
   
-  if (rst_float == T){                                                          #FLOAT
-    if (rst_significant_value < 3.4e+38){
+  if (all(is.logical(minmaxvals)))
+    return("LOG1S")
+  if (rst_float == T) {
+    if (rst_significant_value < 3.4e+38) {
       return("FLT4S")
-    }else
-      return("FLT8S")
+    }
+    else return("FLT8S")
   }
-  
-  if (rst_float == FALSE){                                                      #INT
-    if (rst_signed == TRUE){                                                    #INTS
-      if (rst_significant_value <= 127){
+  if (rst_float == FALSE) {
+    if (rst_signed == TRUE) {
+      if (rst_significant_value <= 127) {
         return("INT2S")
       }
-      if (rst_significant_value <= 32767){
+      if (rst_significant_value <= 32767) {
         return("INT2S")
       }
-      if (rst_significant_value <= 2147483647){
+      if (rst_significant_value <= 2147483647) {
         return("INT4S")
       }
     }
-    if (rst_signed == FALSE){                                                   #INTU
-      if (rst_significant_value <= 254){
+    if (rst_signed == FALSE) {
+      if (rst_significant_value <= 254) {
         return("INT2U")
       }
-      if (rst_significant_value <= 65534){
+      if (rst_significant_value <= 65534) {
         return("INT2U")
       }
-      if (rst_significant_value <= 4294967294){
+      if (rst_significant_value <= 4294967294) {
         return("INT4U")
       }
     }
