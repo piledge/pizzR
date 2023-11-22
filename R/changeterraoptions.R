93b7e192d8d08951502c510ebe37dde630fa381a
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2023-11-22 11:46:44 +0100

    Update changeterraoptions.R

--- R/changeterraoptions.R
+++ R/changeterraoptions.R
@@ -1,42 +1,49 @@
-change.terraOptions <- function (changetempdir = F, tempdir = "", OSRAM.remaining = 3, 
-          progress = 0, verbose = F, ...) 
+change.terraOptions <- function (tempdir = NA,
+                                 progress = 0, verbose = F, ...) 
 {
-  if (!is.logical(changetempdir)) 
-    return(warning("'changetempdir' has to be of class logical!\n"))
-  if (!is.logical(verbose)) 
-    return(warning("'verbose' has to be of class logical!\n"))
+  if (!is.logical(verbose))         return(warning("'verbose' has to be of class logical!\n"))
+  
+  if (!is.na(tempdir)){
+    if (dir.exists(tempdir) == F) {
+                                    base::dir.create(tempdir, recursive = T)
+                                    base::cat(paste("\n", Sys.time(), tempdir, "created"))
+    }
+  }else{
+    if (Sys.info()[1] == 'Windows') tempdir <- 'C:/temp/Rtmp'
+    if (Sys.info()[1] == 'Linux')   tempdir <- '/tmp/Rtmp'
+    if (dir.exists(tempdir) == F) {
+                                    base::dir.create(tempdir, recursive = T)
+                                    base::cat(paste("\n", Sys.time(), tempdir, "created"))
+    }
+  }
+
   package.install <- function(x) {
     to_install <- !x %in% installed.packages()
     if (any(to_install)) {
-      cat(paste0(Sys.time(), ": install missing packages '", 
-                 paste(x[to_install], collapse = ", "), "'\n"))
+      cat(paste0(Sys.time(), ": install missing packages '", paste(x[to_install], collapse = ", "), "'\n"))
       install.packages(x[to_install], dependencies = T)
-      cat(paste0(Sys.time(), ": missing packages '", paste(x[to_install], 
-                                                           collapse = ", "), "' installed\n\n"))
+      cat(paste0(Sys.time(), ": missing packages '", paste(x[to_install], collapse = ", "), "' installed\n\n"))
     }
   }
-  package.install(c("memuse", "raster", "Rcpp", "terra"))
-  require(terra)
-  if (changetempdir && dir.exists(tempdir) == F) {
-    base::dir.create(tempdir, recursive = T)
-    base::cat(paste("\n", Sys.time(), tempdir, "created"))
-  }
-  memmax <- memuse::Sys.meminfo()$totalram@size
-  if (((memmax - OSRAM.remaining)/memmax) > 0.9) {
-    memfrac <- 0.9
-  }
-  else {
-    memfrac <- ((memmax - OSRAM.remaining)/memmax)
-  }
-  fparameters <- list(...)
-  if (changetempdir) 
-    fparameters$tempdir <- tempdir
-  fparameters$memfrac <- memfrac
-  fparameters$memmax <- memmax
-  fparameters$progress <- progress
-  do.call(terra::terraOptions, fparameters)
-  if (verbose) 
-    terra::terraOptions()
-  if (changetempdir) 
-    Sys.setenv(TMP = tempdir, TEMP = tempdir)
+    package.install(c("memuse", "raster", "Rcpp", "terra"))
+    require(terra)
+    
+    memmax <- memuse::Sys.meminfo()$totalram@size
+    if (((memmax - 3)/memmax) > 0.9) {
+      memfrac <- 0.9
+    }
+    else {
+      memfrac <- ((memmax - 3)/memmax)
+    }
+    
+    fparameters <- list(...)
+    if (!is.na(tempdir)){
+      fparameters$tempdir <- tempdir
+      Sys.setenv(TMP = tempdir, TEMP = tempdir)
+    }
+    fparameters$memfrac <- memfrac
+    fparameters$progress <- progress
+    
+    do.call(terra::terraOptions, fparameters)
+    if (verbose) terra::terraOptions()
 }
