0cc35d0e0d71f54f1bf7ad28322a765ead8ca70f
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-01-16 11:38:21 +0100

    Update quapply.R

--- R/quapply.R
+++ R/quapply.R
@@ -5,6 +5,7 @@ quapply <- function(x, qu){
 
   result <- matrix(unlist(lapply(x, quantile, probs = qu)), ncol=ncol(x), byrow = T)
   colnames(result) <- colnames(x)
-  rownames(result) <- qu
+  rownames(result) <- paste0(qu, 'qu')
+
   return(result)
 }
