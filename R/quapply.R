05d14eb7e0966453b7d1e3dd919e667e1d4646a2
piledge <michael.lechner@icloud.com>
2024-01-16 11:01:53 +0100

    quapply added

---
+++ R/quapply.R
@@ -0,0 +1,10 @@
+quapply <- function(x, qu){
+  if (!is.data.frame(sampledata)) return(warning("'x' has to be of class 'dataframe'"))
+  if (!is.numeric(qu)) return(warning("'qu' has to be of class 'numeric'"))
+  if (min(qu) < 0 || max(qu) > 1) return(warning("range of 'qu' has to be from 0 to 1"))
+
+  result <- matrix(unlist(lapply(x, quantile, probs = qu)), ncol=ncol(x), byrow = T)
+  colnames(result) <- colnames(x)
+  rownames(result) <- qu
+  return(result)
+}
