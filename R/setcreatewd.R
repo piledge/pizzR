45de9c693fde6a234e54dd517ea0fb5d1e77df03
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-01-17 15:02:37 +0100

    Update setcreatewd.R

--- R/setcreatewd.R
+++ R/setcreatewd.R
@@ -1,4 +1,5 @@
 setcreate.wd <- function(x){
+  options(digits.secs = 0)
   if (!dir.exists(x)){
     base::dir.create(x, recursive = T)
     base::cat(paste0("\n", Sys.time(),": '", x, "' created and set as working directory"))
