a8a856696a03d1e03253919746359c9eff00bffa
piledge <michael.lechner@icloud.com>
2024-01-18 17:35:55 +0100

    IMGpath can now be of type vector

--- R/otb_namebands.R
+++ R/otb_namebands.R
@@ -1,20 +1,22 @@
 OTB_namebands <- function(IMGpath,savedir=NULL,otbfunction='Haralick'){
-  rst <- terra::rast(IMGpath)
 
-  if (!any(c('tif', 'tiff') %in% tools::file_ext(IMGpath))) return(warning('IMGpath has to be a .tif-file.'))
   if (!otbfunction %in% c('Haralick')) return(warning("Export can currently only be 'Haralick'"))
   if (is.null(savedir)) savedir <- file.path(dirname(IMGpath), 'OTBexport_namebands')
-
-  if (otbfunction == 'Haralick'){
-    nbands <- terra::nlyr(rst)
-    if (!(nbands %in% c(8, 10, 11))) return(warning('Not a valid OTB-Haralick-export'))
-    if (nbands == 8) bandnames <- c("Energy", "Entropy", "Correlation", "InverseDifferenceMoment", "Inertia", "ClusterShade", "ClusterProminence", "HaralickCorrelation")
-    if (nbands == 10) bandnames <- c("Mean", "Variance", "Dissimilarity", "SumAverage", "SumVariance", "SumEntropy", "DifferenceofEntropies", "DifferenceofVariances", "IC1", "IC2")
-    if (nbands == 11) bandnames <- c("ShortRunEmphasis", "LongRunEmphasis", "GreyLevelNonuniformity", "RunLengthNonuniformity", "RunPercentage", "LowGreyLevelRunEmphasis", "HighGreyLevelRunEmphasis", "ShortRunLowGreyLevelEmphasis", "ShortRunHighGreyLevelEmphasis", "LongRunLowGreyLevelEmphasis", "LongRunHighGreyLevelEmphasis")
-  }
-
-  names(rst) <- bandnames
-
   pizzR::setcreate.wd(savedir)
-  pizzR::writeslimRaster(rst, file.path(savedir, basename(IMGpath)))
+
+  for (i in seq(IMGpath)){
+    if (!any(c('tif', 'tiff') %in% tools::file_ext(IMGpath[i]))) return(warning('IMGpath has to be a .tif-file.'))
+    rst <- terra::rast(IMGpath[i])
+
+    if (otbfunction == 'Haralick'){
+      nbands <- terra::nlyr(rst)
+      if (!(nbands %in% c(8, 10, 11))) return(warning('Not a valid OTB-Haralick-export'))
+      if (nbands == 8) bandnames <- c("Energy", "Entropy", "Correlation", "InverseDifferenceMoment", "Inertia", "ClusterShade", "ClusterProminence", "HaralickCorrelation")
+      if (nbands == 10) bandnames <- c("Mean", "Variance", "Dissimilarity", "SumAverage", "SumVariance", "SumEntropy", "DifferenceofEntropies", "DifferenceofVariances", "IC1", "IC2")
+      if (nbands == 11) bandnames <- c("ShortRunEmphasis", "LongRunEmphasis", "GreyLevelNonuniformity", "RunLengthNonuniformity", "RunPercentage", "LowGreyLevelRunEmphasis", "HighGreyLevelRunEmphasis", "ShortRunLowGreyLevelEmphasis", "ShortRunHighGreyLevelEmphasis", "LongRunLowGreyLevelEmphasis", "LongRunHighGreyLevelEmphasis")
+    }
+
+    names(rst) <- bandnames
+    pizzR::writeslimRaster(rst, file.path(savedir, basename(IMGpath[i])))
+  }
 }
