a874d2acfbcf5433ceeaad7a92057408215ec588
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-01 12:55:50 +0100

    Update startfile.R

--- R/startfile.R
+++ R/startfile.R
@@ -1,3 +1,4 @@
+if (length(grep('magrittr', rownames(installed.packages()))) == 0) install.packages("magrittr", dependencies = T)
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     "\nPizzRtime!\n")
