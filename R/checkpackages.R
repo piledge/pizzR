2e6881c9644a03b841e73bdfe55d298553c24e8b
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-01 12:52:39 +0100

    Update checkpackages.R

--- R/checkpackages.R
+++ R/checkpackages.R
@@ -1,10 +1,8 @@
 packages <- c("caret", "crayon", "doParallel", "e1071", "feather", "foreach", "gdata", "ggplot2", "lidR", "magrittr", "memuse", "parallelly", "randomForest", "ranger",
               "raster", "Rcpp", "remotes", "rvest", "RMariaDB", "s2", "sf", "snow", "terra", "tidyverse", "xml2")
 
-if (length(grep('magrittr', rownames(installed.packages()))) == 0) install.packages("magrittr", dependencies = T)
 to_install <- !packages %in% installed.packages()
 if (any(to_install)){
   cat("\n\n\nPackages missing:", paste0(packages[to_install], sep=""), "\n")
   cat("use 'pizzR::package.install(pizzR::packages)' to add\n\n\n\n")
 }
-library("magrittr")
