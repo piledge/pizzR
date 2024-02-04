b9e321f1d4788e511e81db283f080a62ab8e34f0
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-04 20:30:47 +0100

    Update rasterPCA.R

--- R/rasterPCA.R
+++ R/rasterPCA.R
@@ -1,14 +1,6 @@
 rst.pca <- function(x,scale=T){
 
-  package.install <- function(x) {
-    to_install <- !x %in% installed.packages()
-    if (any(to_install)){
-      cat(paste0(pizzR::Systime(), ": install missing packages '", paste(x[to_install], collapse=", "), "'\n"))
-      install.packages(x[to_install], dependencies = T)
-      cat(paste0(pizzR::Systime(), ": missing packages '", paste(x[to_install], collapse=", "), "' installed\n\n"))
-    }
-  }
-  package.install(c("raster", "terra"))
+  pizzR::package.install(c("raster", "terra"), verbose = 1)
   suppressMessages(pizzR::change.terraOptions())
 
   rsttype <- class(x)[1]
