4475691d530bc45ecf113f676203ed780eb4d437
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-04 21:20:07 +0100

    Update changeterraoptions.R

--- R/changeterraoptions.R
+++ R/changeterraoptions.R
@@ -1,41 +1,33 @@
 change.terraOptions <- function (tempdir = NA,
-                                 progress = 0, verbose = F, ...)
+                                 progress = 3, verbose = F, ...)
 {
   if (!is.logical(verbose))         return(warning("'verbose' has to be of class logical!\n"))
-
-  if (!is.na(tempdir)){
-    if (dir.exists(tempdir) == F) {
-                                    base::dir.create(tempdir, recursive = T)
-                                    base::cat(paste("\n", pizzR::Systime(), tempdir, "created"))
-    }
-  }else{
+  if (!is.numeric(progress))        return(warning("'progress' has to be of class integer!\n"))
+  if (is.na(tempdir)){
     if (Sys.info()[1] == 'Windows') tempdir <- 'C:/temp/Rtmp'
     if (Sys.info()[1] == 'Linux')   tempdir <- '/tmp/Rtmp'
-    if (dir.exists(tempdir) == F) {
-                                    base::dir.create(tempdir, recursive = T)
-                                    base::cat(paste("\n", pizzR::Systime(), tempdir, "created"))
-    }
   }
 
-    pizzR::package.install(c("memuse", "raster", "Rcpp", "terra"), verbose = 1)
-    require(terra)
+  pizzR::package.install(c("memuse", "raster", "terra"), verbose = 1)
+  require(terra)
 
-    memmax <- memuse::Sys.meminfo()$totalram@size
-    if (((memmax - 3)/memmax) > 0.9) {
-      memfrac <- 0.9
-    }
-    else {
-      memfrac <- ((memmax - 3)/memmax)
-    }
+  memmax <- memuse::Sys.meminfo()$totalram@size
+  if (((memmax - 3)/memmax) > 0.9) {
+    memfrac <- 0.9
+  }
+  else {
+    memfrac <- ((memmax - 3)/memmax)
+  }
 
-    fparameters <- list(...)
-    if (!is.na(tempdir)){
-      fparameters$tempdir <- tempdir
-      Sys.setenv(TMP = tempdir, TEMP = tempdir)
-    }
-    fparameters$memfrac <- memfrac
-    fparameters$progress <- progress
+  fparameters <- list(...)
+  if (!is.na(tempdir)){
+    pizzR::setcreatewd(tempdir)
+    fparameters$tempdir <- tempdir
+    Sys.setenv(TMP = tempdir, TEMP = tempdir)
+  }
+  fparameters$memfrac  <- memfrac
+  fparameters$progress <- progress
 
-    do.call(terra::terraOptions, fparameters)
-    if (verbose) terra::terraOptions()
+  do.call(terra::terraOptions, fparameters)
+  if (verbose) terra::terraOptions()
 }
