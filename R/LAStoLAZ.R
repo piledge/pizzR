70e41fced192959ed9b0b0d52f9cd66d297b2c9c
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-04 19:21:14 +0100

    Update LAStoLAZ.R

--- R/LAStoLAZ.R
+++ R/LAStoLAZ.R
@@ -1,15 +1,6 @@
 LAStoLAZ <- function(x,y=NULL,verbose=T){
-  package.install <- function(x) {
-    to_install <- !x %in% installed.packages()
-    if (any(to_install)) {
-      cat(paste0(pizzR::Systime(), ": install missing packages '",
-                 paste(x[to_install], collapse = ", "), "'\n"))
-      install.packages(x[to_install], dependencies = T)
-      cat(paste0(pizzR::Systime(), ": missing packages '", paste(x[to_install],
-                                                           collapse = ", "), "' installed\n\n"))
-    }
-  }
-  package.install('lidR')
+
+  pizzR::package.install('lidR')
 
   if(is.null(y)) y <- file.path(dirname(x), 'output_LAStoLAZ')
 
