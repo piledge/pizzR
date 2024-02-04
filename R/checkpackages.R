d8275af23f4bbf04c026edb714de368fd0b84012
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-04 18:59:48 +0100

    Update checkpackages.R

--- R/checkpackages.R
+++ R/checkpackages.R
@@ -1,8 +1,9 @@
 dependencies <- c("caret", "crayon", "doParallel", "e1071", "feather", "foreach", "gdata", "ggplot2", "lidR", "magrittr", "memuse", "parallelly", "randomForest", "ranger",
-              "raster", "Rcpp", "remotes", "rvest", "RMariaDB", "s2", "sf", "snow", "terra", "tidyverse", "xml2")
+                  "raster", "Rcpp", "remotes", "rvest", "RMariaDB", "s2", "sf", "snow", "terra", "tidyverse", "xml2")
 
-to_install <- !dependencies %in% installed.packages()
+to_install <- !dependencies %in% rownames(installed.packages())
 if (any(to_install)){
-  cat("\n\n\nDependencies missing:", paste0(dependencies[to_install], sep=""), "\n")
-  cat("use 'pizzR::package.install(pizzR::dependencies)' to add\n\n\n\n")
+  missing_dependencies <- as.character(dependencies[to_install])
+  cat("\n\nMissing dependencies:", paste(dependencies[to_install], sep = ',', collapse= ", "), "\n")
+  cat("use 'pizzR::package.install(pizzR::dependencies)' to add\n\n")
 }
