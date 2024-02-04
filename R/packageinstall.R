65cd5c5fafd77a382aae1ecb7c7c9366b2bf2e29
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-04 19:13:20 +0100

    Update packageinstall.R

--- R/packageinstall.R
+++ R/packageinstall.R
@@ -1,11 +1,27 @@
-package.install <- function(x=pizzR::packages, verbose = T) {
-  to_install <- !x %in% installed.packages()
+package.install <- function(x=pizzR::dependencies, verbose = F){
+  
+  to_install <- !x %in% rownames(installed.packages())
+  
   if (any(to_install)){
-    cat(paste0("\n", pizzR::Systime(), ": install missing packages '", paste(x[to_install], collapse=", "), "'\n"))
-    install.packages(x[to_install], dependencies = T)
-    cat(paste0("\n", pizzR::Systime(), ": missing packages '", paste(x[to_install], collapse=", "), "' installed\n\n"))
-  }
-  else{
-    if (verbose) cat(paste0("\n", pizzR::Systime(), ": all packages installed\n\n"))
+    missing_packages <- as.character(x[to_install])
+    n.missing <- length(missing_packages)
+    nchar.missing <- nchar(n.missing)
+    cat("\n\nInstall packages:", paste(missing_packages, sep = ',', collapse= ", "), "\n")
+
+    cat("\nInstalling ...\n")
+    for (i in seq(n.missing)){
+      cat(sprintf(paste0("%0", nchar.missing, ".f"), i), 'of', n.missing, ':', missing_packages[i], '\n')
+      install.packages(missing_packages[i], x = F, quiet = !verbose)
+    }
+    
+    success <- (all(x %in% rownames(installed.packages())))
+    if (success) cat("\nDone.\n\n")
+    if (!success){
+      to_install <- !x %in% rownames(installed.packages())
+      missing_packages <- as.character(x[to_install])
+      cat("\n\nPackages not able to install:", paste(missing_packages, sep = ',', collapse= ", "), "\n\n")
+    }
+  }else{
+    cat("\nAll packages are installed\n\n")
   }
 }
