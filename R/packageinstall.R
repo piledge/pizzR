3726ecca3ac1b0fa8c0af0ffc5233a7c1fb0864a
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-04 20:11:38 +0100

    Update packageinstall.R

--- R/packageinstall.R
+++ R/packageinstall.R
@@ -1,17 +1,28 @@
-package.install <- function(x=pizzR::dependencies, verbose = F){
+package.install <- function(x=pizzR::dependencies, verbose = 1){
   
+  if (!is.character(class(x))) return(warning("'x' has to be of class character"))
+  if (!verbose %in% c(0, 1, 2)) return(warning("'verbose' can either be 0 (no messages), 1 (installation status) or 2 (full output)"))
+
   to_install <- !x %in% rownames(installed.packages())
-  
+
   if (any(to_install)){
     missing_packages <- as.character(x[to_install])
-    n.missing <- length(missing_packages)
+    repo_packages <- rownames(available.packages())
+    available_packages <- missing_packages[missing_packages %in% repo_packages]
+    not_to_install <- !missing_packages %in% repo_packages
+    unavailable_packages <- missing_packages[not_to_install]
+    
+    n.missing <- length(available_packages)
     nchar.missing <- nchar(n.missing)
-    cat("\n\nInstall packages:", paste(missing_packages, sep = ',', collapse= ", "), "\n")
-
-    cat("\nInstalling ...\n")
+    if (verbose > 0) cat("\n\nInstall packages:", paste(available_packages, sep = ',', collapse= ", "), "\n")
+    
+    if (verbose > 0) cat("\nInstalling ...\n")
     for (i in seq(n.missing)){
-      cat(sprintf(paste0("%0", nchar.missing, ".f"), i), 'of', n.missing, ':', missing_packages[i], '\n')
-      install.packages(missing_packages[i], x = F, quiet = !verbose)
+      if (verbose > 0) cat(sprintf(paste0("%0", nchar.missing, ".f"), i), 'of', n.missing, ':', available_packages[i], '\n')
+      
+      quiet <- TRUE
+      if (verbose == 2) quiet <- FALSE
+      install.packages(available_packages[i], quiet = quiet)
     }
     
     success <- (all(x %in% rownames(installed.packages())))
@@ -19,9 +30,7 @@ package.install <- function(x=pizzR::dependencies, verbose = F){
     if (!success){
       to_install <- !x %in% rownames(installed.packages())
       missing_packages <- as.character(x[to_install])
-      cat("\n\nPackages not able to install:", paste(missing_packages, sep = ',', collapse= ", "), "\n\n")
+      if (verbose > 0) cat("\n\nPackages not able to install:", paste(missing_packages, sep = ',', collapse= ", "), "\n\n")
     }
-  }else{
-    cat("\nAll packages are installed\n\n")
   }
 }
