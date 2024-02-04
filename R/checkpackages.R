6d940230fe1de285b6b504f0e7c38e85559037c8
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-04 18:30:37 +0100

    Update checkpackages.R

--- R/checkpackages.R
+++ R/checkpackages.R
@@ -4,11 +4,13 @@ dependencies <- c("caret", "crayon", "doParallel", "e1071", "feather", "foreach"
 to_install <- !dependencies %in% rownames(installed.packages())
 
 if (any(to_install)){
-  missing_dependencies <- dependencies[to_install]
+  missing_dependencies <- as.character(dependencies[to_install])
   n.missing <- length(missing_dependencies)
   nchar.missing <- nchar(n.missing)
   
-  cat("\n\nDependencies missing:", paste0(missing_dependencies, sep=","), "\n")
+  if (n.missing == 1) cat("\n\nDependencies missing:", paste0(missing_dependencies, sep=""), "\n")
+  if (n.missing != 1) cat("\n\nDependencies missing:", paste0(missing_dependencies, sep=","), "\n")
+  
   cat("\nInstalling ...\n")
   for (i in seq(n.missing)){
     cat(sprintf(paste0("%0", nchar.missing, ".f"), i), 'of', n.missing, ':', missing_dependencies[i], '\n')
@@ -19,8 +21,11 @@ if (any(to_install)){
   if (success) cat("\n\nDone.\n")
   if (!success){
     to_install <- !dependencies %in% rownames(installed.packages())
-    cat("\n\nDependencies still missing:", dependencies[to_install])
-    cat("\nTry to install manually.\n")
+    missing_dependencies <- as.character(dependencies[to_install])
+    n.missing <- length(missing_dependencies)
+    if (n.missing == 1) cat("\n\nDependencies still missing:", paste0(missing_dependencies, sep=""), "\n")
+    if (n.missing != 1) cat("\n\nDependencies still missing:", paste0(missing_dependencies, sep=","), "\n")
+    cat("\nTry to install manually.\n\n")
   }
 }else{
   cat("\nDependencies meet the requirements\n")
