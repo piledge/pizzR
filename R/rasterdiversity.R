2166be2ca703fd0240f4715e33f4e5936ed4105b
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-04 20:31:43 +0100

    Update rasterdiversity.R

--- R/rasterdiversity.R
+++ R/rasterdiversity.R
@@ -7,15 +7,7 @@ rasterdiversity <- function(x, index='shannon', window=3, ...){
   if (window.divided - floor(window.divided) != 0.5)                                                             return(warning("'window' has to be a odd integer\n"))
   if (window > terra::nrow(x) && window > terra::ncol(x))                                                        return(warning("Focal window bigger than raster!\n"))
 
-  package.install <- function(x) {
-    to_install <- !x %in% installed.packages()
-    if (any(to_install)){
-      cat(paste0(pizzR::Systime(), ": install missing packages '", paste(x[to_install], collapse=", "), "'\n"))
-      install.packages(x[to_install], dependencies = T)
-      cat(paste0(pizzR::Systime(), ": missing packages '", paste(x[to_install], collapse=", "), "' installed\n\n"))
-    }
-  }
-  package.install(c("raster", "Rcpp", "terra"))
+  pizzR::package.install(c("raster", "terra"), verbose = 1)
 
   library(terra)
 
