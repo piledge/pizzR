fe847f09280c1b404daf294c7562473ed6c4ef85
pi-ledge <michael.lechner@icloud.com>
2024-02-07 11:32:45 +0100

    bug fixed for return of FLT4S

--- R/optdatatype.R
+++ R/optdatatype.R
@@ -1,26 +1,26 @@
-opt.datatype <- function(x) {
-
+opt.datatype <- function (x)
+{
   pizzR::package.install(c("memuse", "raster", "terra"), verbose = 1)
-
   rsttype <- class(x)[1]
   if (rsttype != "SpatRaster" && rsttype != "RasterLayer" &&
       rsttype != "RasterBrick" && rsttype != "RasterStack")
     return(warning("Not a suitable rasterfile!\n"))
-
-  if (rsttype == "SpatRaster"){
+  if (rsttype == "SpatRaster") {
     terra::setMinMax(x, force = T)
     minmaxvals <- terra::minmax(x)
-    }
-  if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack")   minmaxvals <- raster::minValue(x)
-
+    vals.sample <- terra::spatSample(x, size=100, na.rm=T)
+  }
+  if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack"){
+    minmaxvals <- raster::minValue(x)
+    vals.sample <- raster::sampleRandom(x, size=100, na.rm=T)
+  }
   rst_min <- min(minmaxvals)
   rst_max <- max(minmaxvals)
   rst_significant_value <- max(abs(c(rst_min, rst_max)))
   rst_signed <- rst_min < 0
   rst_float <- TRUE
-
-  if(all((floor(minmaxvals)/minmaxvals) == 1, na.rm = T)) rst_float <- FALSE
-
+  if (all((floor(vals.sample)/vals.sample) == 1, na.rm = T))
+    rst_float <- FALSE
   if (all(is.logical(minmaxvals)))
     return("LOG1S")
   if (rst_float == T) {
