57b96620120fb7e3370b239ffee4e8e2ed682fbb
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-08 19:25:57 +0100

    Update L1_trajectory_to_shapefile.R

--- R/L1_trajectory_to_shapefile.R
+++ R/L1_trajectory_to_shapefile.R
@@ -15,7 +15,7 @@ L1_trajectory_to_shapefile <- function(x=NULL,y=NULL,reduce=T,crs.origin=4326,cr
   header_names   <- scan(traject_file, what = '', nlines = 1, sep='')
   data           <- suppressWarnings(read.table(x, header = F, sep='', skip=2))
   colnames(data) <- header_names[-1]
-  data.points <- nrow(data)
+  data.points    <- nrow(data)
 
   if (is.logical(reduce) | is.numeric(reduce))  cat(paste0('\n', pizzR::Systime(), ': Subsetting ...'))
   if (reduce){
