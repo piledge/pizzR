3640c11a762d8d82559dacad05b7690b218773c9
piledge <michael.lechner@icloud.com>
2024-02-15 12:24:40 +0100

    semiparallel extract

---
+++ R/semiparallel_extract.R
@@ -0,0 +1,23 @@
+semiparallel.extract <- function(SpatRaster=NULL, SpatVector=NULL, core=1, ncores=2, opath=NULL){
+
+  if (class(SpatRaster)[1] != "SpatRaster") return(warning("Not a suitable rasterfile!\n"))
+  if (class(SpatVector)[1] != "SpatVector") return(warning("Not a suitable vectorfile!\n"))
+
+  chunks.id <- pizzR::split.vector(seq(shp), groups = ncores)
+  chunks <- list()
+  for (i in seq(ncores)) chunks[[i]] <- shp[chunks.id == i]
+
+  extr <- terra::extract(rst, chunks[[core]])
+
+  nobjects <- as.numeric(table(chunks.id))
+  nobjects <- c(0, nobjects[-length(nobjects)])
+  newid <- cumsum(nobjects)
+
+  extr$ID <- extr$ID + newid[core]
+
+  filename <- paste0(sprintf(paste0('extract_%0', nchar(ncores), '.f'), core), '.feather')
+  if (is.null(opath)) pizzR::setcreate.wd(file.path(tempdir(), 'parallel_extract')) else pizzR::setcreate.wd(opath)
+
+  feather::write_feather(extr, filename)
+  cat(paste0("\n", pizzR::Systime(), ": '",filename, "' written to '", getwd(), "'/n"))
+}
