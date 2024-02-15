668359d57c9c2b1fc52a718e042533eb69a59549
pi-ledge <70260300+pi-ledge@users.noreply.github.com>
2024-02-15 14:14:47 +0100

    Update combine_semiparallel_extract.R

--- R/combine_semiparallel_extract.R
+++ R/combine_semiparallel_extract.R
@@ -1,6 +1,8 @@
 combine.semiparallel.extract <- function(ipath){
   ffiles <- list.files(ipath, pattern = '.feather', full.names = T)
 
+  pizzR::package.install(c("feather"), verbose = 1)
+
   data <- vector('list', length(ffiles))
   for (i in seq(ffiles)) data[[i]] <- as.data.frame(feather::read_feather(ffiles[i]))
 
