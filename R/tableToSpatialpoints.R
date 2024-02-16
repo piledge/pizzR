3ba60aafe399cf63a658fd1d3ebaf0878a932e04
piledge <michael.lechner@icloud.com>
2024-02-16 11:04:42 +0100

    update

--- R/tableToSpatialpoints.R
+++ R/tableToSpatialpoints.R
@@ -1,12 +1,19 @@
-tableToSpatialpoints <- function(northing,easting,crs.origin,crs.project=NULL,attributes=NULL,filename=NULL,overwrite=T,plot=FALSE,filetype=NULL,layer=NULL,opath=NULL,options="ENCODING=UTF-8",...){
+tableToSpatialpoints <- function(northing,easting,crs.origin,crs.project=NULL,attributes=NULL,filename=NULL,overwrite=T,plot=FALSE,filetype=NULL,layer=NULL,write=F,opath=NULL,options="ENCODING=UTF-8",...){
 
   pizzR::package.install(c("sp", "raster", "terra"), verbose = 1)
 
   table_xy <- data.frame(latitude=suppressWarnings(as.numeric(northing)), longitude=suppressWarnings(as.numeric(easting)))
 
-  if (any(is.na(table_xy$longitude)) && any(is.na(table_xy$latitude))) return(warning("Northing- and Easting contain non-numeric values"))
-  if (any(is.na(table_xy$longitude)))                                  return(warning("Northing contains non-numeric values"))
-  if (any(is.na(table_xy$latitude)))                                   return(warning("Easting contains non-numeric values"))
+  if (any(is.na(table_xy$longitude)) && any(is.na(table_xy$latitude)))  return(warning("Northing- and Easting contain non-numeric values"))
+  if (any(is.na(table_xy$longitude)))                                   return(warning("Northing contains non-numeric values"))
+  if (any(is.na(table_xy$latitude)))                                    return(warning("Easting contains non-numeric values"))
+  if (!is.logical(overwrite))                                           return(warning("'overwrite' has to be of type logical!"))
+  if (!is.logical(plot))                                                return(warning("'plot' has to be of type logical!"))
+  if (!is.logical(write))                                               return(warning("'write' has to be of type logical!"))
+
+  if (!write && is.null(opath))   opath <- tempdir()
+  if (write && is.null(opath))    opath <- getwd()
+  if (!write && !is.null(opath))  opath <- tempdir()
 
   if (!is.null(attributes)) data <- cbind(table_xy, attributes) else data <- table_xy
 
@@ -25,8 +32,8 @@ tableToSpatialpoints <- function(northing,easting,crs.origin,crs.project=NULL,at
     crs.export <- crs.project
   }
 
-  if (!is.null(filename)) filename <- filename else filename <- paste0("points_", crs.export, ".shp")
-  if (plot == T) terra::plot(shp, main=paste0('CRS: ', crs.export))
+  if (!is.null(filename)) filename <- file.path(opath, filename) else filename <- file.path(opath, paste0("points_", crs.export, ".shp"))
+  if (plot) terra::plot(shp, main=paste0('CRS: ', crs.export))
 
   fparameters             <- list(...)
   fparameters$x           <- shp
@@ -37,6 +44,8 @@ tableToSpatialpoints <- function(northing,easting,crs.origin,crs.project=NULL,at
   fparameters$options     <- options
 
   do.call(terra::writeVector, fparameters)
-  if (is.null(opath)) pizzR::setcreate.wd(tempdir())
-  cat(paste0("\n", pizzR::Systime(), ": '",filename, "' written to '", getwd(), "'\n"))
+
+  if (write) cat(paste0("\n", pizzR::Systime(), ": '",filename, "' written to '", dirname(filename), "'\n"))
+
+  invisible(terra::vect(filename))
 }
