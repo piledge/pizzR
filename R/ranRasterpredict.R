96ffa585c3987bd9721a6df5e3fbd0e8e0e0928f
piledge <michael.lechner@icloud.com>
2024-02-27 10:58:16 +0100

    update

--- R/ranRasterpredict.R
+++ R/ranRasterpredict.R
@@ -1,16 +1,20 @@
-ranRasterpredict <- function(rasterobject, ranger, na.rm = F, ...){
+ranRasterpredict <- function(rasterobject, ranger, na.rm = F, visualize=F, ...){
   pizzR::package.install(c("ranger", "raster", "terra"), verbose = 1)
 
   if (class(rasterobject)[1] != "SpatRaster") return(warning("Not a suitable rasterfile!\n"))
   if (class(ranger) != "ranger") return(warning("Not a suitable ranger-model!\n"))
   if (!is.logical(na.rm)) return(warning("'na.rm' needs to be boolean!\n"))
+  if (!is.logical(plot)) return(warning("'plot' needs to be boolean!\n"))
 
   dots        <- list(...)
   dots$object <- rasterobject
   dots$model  <- ranger
-  dots$fun   <- \(...)  predict(...)$predictions
+  dots$fun    <- \(...)  predict(...)$predictions
+  dots$na.rm  <- na.rm
 
   pred <- do.call(terra::predict, dots)
+
+  if (visualize) terra::plot(pred)
 
   return(pred)
 }
