6a1af9ce0c800eab20d679b991404ff3d94190d9
pi-ledge <michael.lechner@icloud.com>
2024-03-04 11:04:52 +0100

    update

--- R/rgdal_install.R
+++ R/rgdal_install.R
@@ -1,4 +1,15 @@
-rgdal.install <- function(url=NULL){
+rgdal.install <- function(url=NULL, quiet = T){
+  if (!is.logical(quiet))         return(warning("'quiet' has to be of class logical!\n"))
+
   if (is.null(url)) url <- 'https://cran.r-project.org/src/contrib/Archive/rgdal/rgdal_1.6-7.tar.gz'
-  install.packages(url, type = 'source', repos = NULL)
+  install.packages(url, type = 'source', repos = NULL, quiet = quiet)
+
+  successful <- 'rgdal' %in% rownames(installed.packages())
+  if (successful)   warning('Depreciated rgdal-package successfully installed! Use alternatives sf and terra instead!')
+  if (!successful)  warning('Error during installation occured')
 }
+
+rgdal.install()
+
+
+
