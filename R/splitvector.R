49eea35b2d9d2d648a2f73571eff114936a402ce
piledge <70260300+piledge@users.noreply.github.com>
2024-10-05 23:22:29 +0200

    Update splitvector.R

--- R/splitvector.R
+++ R/splitvector.R
@@ -1,19 +1,17 @@
-split.vector <- function (vector, groups) 
-{
+split.vector <- function(vector, groups) {
   max.range <- max(vector)
   nvals <- length(vector)
-  size.main <- floor(max.range/groups)
-  size.last <- max.range - sum(rep(size.main, groups - 1))
+
+  size.main <- floor(max.range / groups)
+  size.last <- max.range - (size.main * (groups - 1))
   lengths <- c(rep(size.main, groups - 1), size.last)
-  splits <- split(vector, rep(seq(groups), lengths))
-  for (i in seq(groups - 1)) splits[[as.character(i)]] <- rep(i, size.main)
-  splits[[groups]] <- rep(groups, size.last)
-  
-  if (groups == 1){
-    result <- splits[[1]]
-  }else{
-    result <- append(splits[[1]], splits[[2]])
-    if (groups > 2) for (i in 3:groups) result <- append(result, splits[[i]])
+
+  group_labels <- rep(1:groups, lengths)
+
+  result <- rep(NA, nvals)
+  for (i in 1:groups) {
+    result[group_labels == i] <- i
   }
+
   return(result)
 }
