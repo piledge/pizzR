f7c544bd5104bb5fd88c6f4c3679e56d7b42a5bb
piledge <70260300+piledge@users.noreply.github.com>
2024-10-05 23:58:28 +0200

    Update LAStoLAZ.R

--- R/LAStoLAZ.R
+++ R/LAStoLAZ.R
@@ -1,26 +1,22 @@
-LAStoLAZ <- function(x, y = NULL, verbose = TRUE) {
-  pizzR::package.install('lidR', verbose = 1)
+LAStoLAZ <- function(x, y = NULL, verbose = TRUE){
 
+  pizzR::package.install('lidR', verbose = 1)
+  
   if (is.null(y)) y <- file.path(dirname(x), 'output_LAStoLAZ')
   if (!dir.exists(y)) pizzR::setcreate.wd(y)
-
   stopifnot(is.logical(verbose))
   stopifnot(dir.exists(x), msg = 'Input-folder does not exist!')
-  
   lasfiles <- list.files(x, pattern = '\\.las$', full.names = TRUE)
   nfiles <- length(lasfiles)
-
   stopifnot(nfiles > 0, msg = 'Input-folder empty!')
 
   if (verbose) cat(paste0('\nWriting LAZ-files to "', y, '"\n\n'))
-
   for (i in seq_along(lasfiles)) {
     if (verbose) cat('Compressing: ', i, '/', nfiles, '   \r')
-
+    
     las <- lidR::readLAS(lasfiles[i])
     output_file <- file.path(y, sub('\\.las$', '.laz', basename(lasfiles[i])))
     lidR::writeLAS(las, output_file)
   }
-  
   if (verbose) cat('\nCompression complete!\n')
 }
