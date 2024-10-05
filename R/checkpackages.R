f4ebad4e9fe24fdbdaaf336f97260ca858af0a46
piledge <70260300+piledge@users.noreply.github.com>
2024-10-05 23:52:22 +0200

    Update checkpackages.R

--- R/checkpackages.R
+++ R/checkpackages.R
@@ -1,9 +1,16 @@
-dependencies <- c("caret", "crayon", "dbscan", "doParallel", "e1071", "feather", "foreach", "gdata", "ggplot2", "lidR", "magrittr", "memuse", "parallelly", "randomForest", "ranger",
-                  "raster", "Rcpp", "remotes", "rvest", "RMariaDB", "s2", "sf", "snow", "terra", "tidyverse", "xml2")
+dependencies <- c("caret", "crayon", "dbscan", "doParallel", "e1071", 
+                  "feather", "foreach", "gdata", "ggplot2", "lidR", 
+                  "magrittr", "memuse", "parallelly", "randomForest", 
+                  "ranger", "raster", "Rcpp", "remotes", "rvest", 
+                  "RMariaDB", "s2", "sf", "snow", "terra", "tidyverse", 
+                  "xml2")
 
 to_install <- !dependencies %in% rownames(installed.packages())
-if (any(to_install)){
+
+if (any(to_install)) {
   missing_dependencies <- as.character(dependencies[to_install])
-  cat("\n\nMissing dependencies:", paste(dependencies[to_install], sep = ',', collapse= ", "), "\n")
-  cat("use 'pizzR::package.install(pizzR::dependencies)' to add\n\n")
+  cat("\n\nMissing dependencies:", paste(missing_dependencies, collapse = ", "), "\n")
+  cat("Use 'pizzR::package.install(pizzR::dependencies)' to add missing packages.\n\n")
+} else {
+  cat("All dependencies are installed.\n")
 }
