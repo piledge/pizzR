23bab29bb3b1b05fd32529897fd167449e144789
piledge <70260300+piledge@users.noreply.github.com>
2024-10-05 23:50:40 +0200

    Update setcreatewd.R

--- R/setcreatewd.R
+++ R/setcreatewd.R
@@ -1,14 +1,17 @@
-setcreate.wd <- function(path, verbose=T){
-  if (!is.logical(verbose)) return(warning("'verbose' has to be of type logical!"))
+setcreate.wd <- function(path, verbose = TRUE) {
+  stopifnot(is.logical(verbose))
 
-  if (!dir.exists(path)){
-    dir.create(path, recursive = T)
-    if (verbose)  cat(paste0("\n", pizzR::Systime(),": '", path, "' created and set as working directory"))
+  if (!dir.exists(path)) {
+    dir.create(path, recursive = TRUE)
+    if (verbose) {
+      cat(paste0("\n", pizzR::Systime(), ": '", path, "' created and set as working directory"))
+    }
+  } 
+
+  if (getwd() != path) {
     setwd(path)
-  } else{
-    if (getwd() != path){
-      if (verbose)  cat(paste0("\n", pizzR::Systime(),": '", path, "' set as working directory"))
-      setwd(path)
+    if (verbose) {
+      cat(paste0("\n", pizzR::Systime(), ": '", path, "' set as working directory"))
     }
   }
 }
