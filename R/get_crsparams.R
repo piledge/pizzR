e9f55735fb3d5229a69e976141459ceac1e519ad
piledge <70260300+piledge@users.noreply.github.com>
2024-10-05 23:31:15 +0200

    Update get_crsparams.R

--- R/get_crsparams.R
+++ R/get_crsparams.R
@@ -1,7 +1,17 @@
-get.crsparams <- function(epsg.crs=NULL){
-  pizzR::package.install(c("rvest", "xml2"), verbose = 1)
+get.crsparams <- function(epsg.crs = NULL) {
+  if (is.null(epsg.crs) || !is.numeric(epsg.crs)) {
+    stop("Bitte einen gültigen EPSG-Code angeben.")
+  }
 
+  pizzR::package.install(c("rvest", "xml2"), verbose = 1)
+  
   crs.link <- paste0("https://spatialreference.org/ref/epsg/", epsg.crs, "/proj4.txt")
-  crs.params <- rvest::html_text(xml2::read_html(crs.link))
+  
+  tryCatch({
+    crs.params <- rvest::html_text(xml2::read_html(crs.link))
+  }, error = function(e) {
+    stop("Fehler beim Abrufen der CRS-Parameter: ", e$message)
+  })
+  
   return(crs.params)
 }
