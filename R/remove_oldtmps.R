dcaa28d7d885a92e34c1d8dc573f3c58df0ad04e
piledge <70260300+piledge@users.noreply.github.com>
2024-10-09 14:52:55 +0200

    Update remove_oldtmps.R

--- R/remove_oldtmps.R
+++ R/remove_oldtmps.R
@@ -3,22 +3,22 @@ remove.oldtmps <- function(recursive=T, force=F, expand=T){
             inherits(force, 'logical'),
             inherits(expand, 'logical'))
   
-  tmp.dir.current <- tempdir()
-  tmp.folder <- dirname(tmp.dir.current)
+  tmp_dir_current <- tempdir()
+  tmp_folder <- dirname(tmp_dir_current)
   
-  tmp.folders <- dir(tmp.folder, pattern = 'Rtmp', full.names = T)
-  tmp.folders.old <- tmp.folders[-grep(basename(tmp.dir.current), tmp.folders)]
-  tmp.files <- list.files(tmp.folders.old, recursive = T, full.names = T)
-  filesum <- pizzR::format_file_size(sum(file.size(tmp.files)))
+  tmp_folders <- dir(tmp_folder, pattern = 'Rtmp', full.names = T)
+  tmp_folders_old <- tmp_folders[-grep(basename(tmp_dir_current), tmp_folders)]
+  tmp_files <- list.files(tmp_folders_old, recursive = T, full.names = T)
+  filesum <- pizzR::format_file_size(sum(file.size(tmp_files)))
   
-  n.folders.old <- length(tmp.folders.old)
+  n_folders_old <- length(tmp_folders_old)
   
-  if (n.folders.old == 0){
+  if (n_folders_old == 0){
     cat(sprintf("\n%s: no old folders have been removed!\n", pizzR::Systime()))
   }else{
-    unlink(tmp.folders.old, recursive = recursive, force = force, expand = expand)
+    unlink(tmp_folders_old, recursive = recursive, force = force, expand = expand)
     
-    cat(sprintf("\n%s: %d old folders have been removed!", pizzR::Systime(), n.folders.old))
+    cat(sprintf("\n%s: %d old folders have been removed!", pizzR::Systime(), n_folders_old))
     cat(sprintf("\n%s: %s of storage have been freed!\n", pizzR::Systime(), filesum))
   }
 }
