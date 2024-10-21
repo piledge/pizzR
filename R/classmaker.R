6ddbe4452952f0315f21970d6d9efb8cd0462b96
piledge <70260300+piledge@users.noreply.github.com>
2024-10-21 09:56:32 +0200

    Create classmaker.R

---
+++ R/classmaker.R
@@ -0,0 +1,8 @@
+classmaker <- function(x, class_range = 50, include.lowest = TRUE, right = FALSE) {
+  max_value <- ceiling(max(x) / class_range) * class_range
+  breaks <- seq(0, max_value, by = class_range)
+  classes <- as.integer(cut(x, breaks = breaks, include.lowest = include.lowest, right = right))
+  classes[is.na(classes)] <- 0
+  
+  return(classes)
+}
