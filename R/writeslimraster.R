7f672e1142db9a232d2c6c3dc3252685f2b14646
piledge <70260300+piledge@users.noreply.github.com>
2024-11-05 15:22:05 +0100

    Update writeslimraster.R
    inherited classes after reclass

--- R/writeslimraster.R
+++ R/writeslimraster.R
@@ -1,54 +1,61 @@
-writeslimRaster <- function(rasterobject, filename, compression=T, overwrite=T, BIGTIFF="YES", filetype="GTiff", datatype="ESTIMATE", ...){
+function (x) 
+{
+  pizzR::package.install(c("memuse", "raster", "terra"), verbose = 1)
+  rsttype <- class(x)[1]
+  stopifnot(rsttype %in% c("SpatRaster", "RasterLayer", "RasterBrick", 
+                           "RasterStack"))
+  if (rsttype == "SpatRaster") {
+    terra::setMinMax(x, force = T)
+    minmaxvals <- terra::minmax(x)
+    vals_sample <- as.matrix(terra::spatSample(x, size = 100, 
+                                               na.rm = T))
+  }
+  else {
+    minmaxvals <- raster::minValue(x)
+    vals_sample <- as.matrix(raster::sampleRandom(x, size = 100, 
+                                                  na.rm = T))
+  }
+  if (any(is.logical(vals_sample))) 
+    return("LOG1S")
 
-  pizzR::package.install(c("raster", "terra"), verbose = 1)
-
-  rsttype <- class(rasterobject)[1]
-  stopifnot(
-    rsttype %in% c("SpatRaster", "RasterLayer", "RasterBrick", "RasterStack"),
-    is.logical(compression),
-    is.logical(overwrite),
-    BIGTIFF %in% c("YES", "NO", "IF_SAFER", "IF_NEEDED"),
-    datatype %in% c("LOG1S", "INT1S", "INT1U", "INT2S", "INT2U", "INT4S", "INT4U", "FLT4S", "FLT8S", "ESTIMATE")
-  )
-
-  fparameters             <- list(...)
-  fparameters$filename    <- filename
-  fparameters$overwrite   <- overwrite
-  fparameters$filetype    <- filetype
-  if (datatype != "ESTIMATE") fparameters$datatype <- datatype
-  
-  if (rsttype == "SpatRaster"){
-
-    if (compression && datatype == "ESTIMATE"){
-      cat(sprintf("\n%s: Estimate datatype ...", pizzR::Systime()))
-      fparameters$datatype                                                      <- pizzR::opt.datatype(rasterobject)
+  if (inherits(vals_sample, c('matrix', 'array'))){
+    n_classes <- length(unique(test))
+    if (n_classes < 65534){
+      return("INT2U") 
     }
-    if (compression){
-      fparameters$gdal <- c(sprintf("BIGTIFF = %s", BIGTIFF), "COMPRESS = DEFLATE", "ZLEVEL = 9", "PREDICTOR = 2")
-      cat(sprintf("\n%s: Write slim rasterfile as '%s' ...\n", pizzR::Systime(), fparameters$datatype))
-    }else{
-      fparameters$gdal <- c(sprintf("BIGTIFF = %s", BIGTIFF))
-      cat(sprintf("\n%s: Write rasterfile ...\n", pizzR::Systime()))
-    }
-
-    terra::setMinMax(rasterobject, force = T)
-    fparameters$x                                                               <- rasterobject
-    do.call(terra::writeRaster, fparameters)
+    else {
+        return("INT4U")
+      }
   }
 
-  if (rsttype == "RasterLayer" || rsttype == "RasterBrick" || rsttype == "RasterStack"){
-
-    if (compression && datatype == "ESTIMATE"){
-      cat(sprintf("\n%s: Estimate datatype ...", pizzR::Systime()))
-      fparameters$datatype                                                      <- pizzR::opt.datatype(x, samplesize)
+  rst_min <- min(minmaxvals, na.rm = TRUE)
+  rst_max <- max(minmaxvals, na.rm = TRUE)
+  rst_significant_value <- max(abs(c(rst_min, rst_max)), na.rm = TRUE)
+  rst_signed <- rst_min < 0
+  rst_float <- !all((floor(vals_sample) == vals_sample), na.rm = TRUE)
+  if (rst_float) 
+    return(ifelse(rst_significant_value < 3.4e+38, "FLT4S", 
+                  "FLT8S"))
+  if (rst_signed) {
+    if (rst_significant_value <= 127) {
+      return("INT2S")
     }
-    if (compression && datatype != "ESTIMATE") fparameters$datatype     <- datatype
-    if (compression) fparameters$options                                <- c("COMPRESS=DEFLATE", "PREDICTOR=2", "ZLEVEL=9")
-
-    if (compression) cat(sprintf("\n%s: Write slim rasterfile as '%s' ...\n", pizzR::Systime(), fparameters$datatype)) else cat(sprintf("\n%s: Write rasterfile ...\n", pizzR::Systime()))
-
-    fparameters$x                                                               <- rasterobject
-    do.call(raster::writeRaster, fparameters)
+    else if (rst_significant_value <= 32767) {
+      return("INT2S")
+    }
+    else if (rst_significant_value <= 2147483647) {
+      return("INT4S")
+    }
   }
-  cat(sprintf("%s: Done ...\n", pizzR::Systime()))
+  else {
+    if (rst_significant_value <= 254) {
+      return("INT2U")
+    }
+    else if (rst_significant_value <= 65534) {
+      return("INT2U")
+    }
+    else if (rst_significant_value <= 4294967294) {
+      return("INT4U")
+    }
+  }
 }
