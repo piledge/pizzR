731f996ba29c0256120b537bdc10e49fed4ebab7
piledge <70260300+piledge@users.noreply.github.com>
2024-11-12 10:41:47 +0100

    Update loop_progress.R

--- R/loop_progress.R
+++ R/loop_progress.R
@@ -1,3 +1,3 @@
-loop_progress <- function(i, total_digits, text = 'Loop'){
+loop_progress <- function(i, total_digits = 5, text = 'Loop'){
   cat(sprintf("%s: %s %0*d\n", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), text, total_digits, i))
 }
