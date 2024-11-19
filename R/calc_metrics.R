ac0c64575a12975589fe1a3024c85c13cdbcbca5
piledge <70260300+piledge@users.noreply.github.com>
2024-11-19 15:07:29 +0100

    Update calc_metrics.R

--- R/calc_metrics.R
+++ R/calc_metrics.R
@@ -1,4 +1,4 @@
-calc_metrics <- function (data, id = "ID")
+calc_metrics <- function (data, id = "ID", verbose = TRUE)
 {
   id_col <- which(names(data) == id)
   unique_ids <- unique(data[[id]])
@@ -14,7 +14,7 @@ calc_metrics <- function (data, id = "ID")
   res <- data.frame((matrix(NA, nrow = n_samples, ncol = length(metrics_names_rep))))
   colnames(res) <- metrics_names_rep
   for (i in seq_along(unique_ids)) {
-    pizzR::loop_progress(i, total_digits = nchar_n_samples)
+    if (verbose) pizzR::loop_progress(i, total_digits = nchar_n_samples)
     data_subs <- na.omit(data[data$ID == i, -id_col])
     metrics <- matrix(rbind(pizzR::quapply(data_subs, 0.05),
                             pizzR::quapply(data_subs, 0.1),
