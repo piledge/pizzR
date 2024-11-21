815868c216b7bf84f1965c2db415fc9b016892d6
piledge <70260300+piledge@users.noreply.github.com>
2024-11-21 13:57:06 +0100

    Update translate.R

--- R/translate.R
+++ R/translate.R
@@ -3,12 +3,12 @@ translate <- function(x,dictionary=NULL,verbose=F){
   stopifnot(!is.null(dictionary),
             all(c('new', 'old') %in% colnames_dictionary),
             is.logical(verbose))
-
+  
   loops <- seq_len(nrow(dictionary))
-  nchar.loops <- nchar(max(loops))
+  nchar_loops <- nchar(max(loops))
   colnr_old <- grep("old", colnames_dictionary)
   colnr_new <- grep("new", colnames_dictionary)
-
+  
   if (!verbose){
     for (i in loops){
       x[x == dictionary[i, colnr_old]] <- dictionary[i, colnr_new]
@@ -19,8 +19,7 @@ translate <- function(x,dictionary=NULL,verbose=F){
       cat(sprintf("\r %s: remaining items to translate: %*d", pizzR::Systime(), nchar_loops, loops - i + 1))
       x[x == dictionary[i, colnr_old]] <- dictionary[i, colnr_new]
     }
+    cat(sprintf("\r %s: %*d items translated             \n", pizzR::Systime(), nchar_loops, max(loops)))
   }
-  if (verbose) cat(sprintf("\r %s: %*d items translated\n", pizzR::Systime(), nchar_loops + 13, max(loops)))
   return(x)
 }
-
