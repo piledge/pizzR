6470bebc661e007177e5f46b91b6be1471489d54
piledge <michael.lechner@icloud.com>
2024-11-25 14:14:18 +0100

    refactor

--- R/get_crsparams.R
+++ R/get_crsparams.R
@@ -1,17 +1,17 @@
 get.crsparams <- function(epsg.crs = NULL) {
   if (is.null(epsg.crs) || !is.numeric(epsg.crs)) {
-    stop("Bitte einen gültigen EPSG-Code angeben.")
+    stop("Please enter a valid EPSG-Code")
   }
 
   pizzR::package.install(c("rvest", "xml2"), verbose = 1)
-  
+
   crs.link <- paste0("https://spatialreference.org/ref/epsg/", epsg.crs, "/proj4.txt")
-  
+
   tryCatch({
     crs.params <- rvest::html_text(xml2::read_html(crs.link))
   }, error = function(e) {
-    stop("Fehler beim Abrufen der CRS-Parameter: ", e$message)
+    stop("Error when retrieving the CRS parameters: ", e$message)
   })
-  
+
   return(crs.params)
 }
