416d4017362a9a6a0f527bc26e98b490ffab2d7e
piledge <michael.lechner@icloud.com>
2024-11-25 14:38:38 +0100

    update

--- R/setcreatewd.R
+++ R/setcreatewd.R
@@ -3,11 +3,11 @@ setcreate.wd <- function(path, verbose=T){
 
   if (!dir.exists(path)){
     dir.create(path, recursive = T)
-    if (verbose)  cat(paste0("\n", pizzR::Systime(),": '", path, "' created and set as working directory"))
+    if (verbose)  cat(sprintf("\n%s: '%s' created and set as working directory.", pizzR::Systime(), path))
     setwd(path)
   } else{
     if (getwd() != path){
-      if (verbose)  cat(paste0("\n", pizzR::Systime(),": '", path, "' set as working directory"))
+      if (verbose)  cat(sprintf("\n%s: '%s' set as working directory.", pizzR::Systime(), path))
       setwd(path)
     }
   }
