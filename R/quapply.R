b16f4dd8597da0bb9c11c4bdc3d560027d819597
piledge <michael.lechner@icloud.com>
2024-11-25 15:17:11 +0100

    removed unecessary catches

--- R/quapply.R
+++ R/quapply.R
@@ -1,10 +1,4 @@
 quapply <- function(x, qu){
-  stopifnot(
-    is.data.frame(x),
-    is.numeric(qu),
-    all(qu >= 0 & qu <= 1)
-  )
-
   result <- matrix(unlist(lapply(x, quantile, probs = qu)), ncol=ncol(x), byrow = T)
   colnames(result) <- colnames(x)
   rownames(result) <- paste0(qu, 'qu')
