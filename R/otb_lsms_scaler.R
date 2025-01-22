024a73a3da90c33a73f04c1a8e3d5f31586b47ed
piledge <70260300+piledge@users.noreply.github.com>
2025-01-22 16:48:46 +0100

    Update otb_lsms_scaler.R

--- R/otb_lsms_scaler.R
+++ R/otb_lsms_scaler.R
@@ -1,27 +1,28 @@
 OTB_lsms_scaler <- function (rasterobject = NULL, maxval = 255, truncate = F)
 {
   pizzR::package.install(c("raster", "terra"), verbose = 1)
-
+  
   rsttype <- class(rasterobject)[1]
   stopifnot(rsttype == "SpatRaster")
   stopifnot(is.logical(truncate))
   stopifnot(is.numeric(maxval))
-
+  
   n.bands <- terra::nlyr(rasterobject)
   n.bands.chars <- nchar(n.bands)
-
+  
   cat(sprintf("\n\n%s: Scale %d bands ...\n", pizzR::Systime(), n.bands))
-
+  
   terra::setMinMax(rasterobject, force = T)
   rst.minmax <- terra::minmax(rasterobject)
   for (i in seq(n.bands)) {
+    cat(sprintf("\n%s: Scale band %0*d of %d", pizzR::Systime(), n.bands.chars, i, n.bands))
     minval <- rst.minmax[1, i]
     maxval <- rst.minmax[2, i]
     fact <- 1/((maxval - minval)) * 255
     rasterobject[[i]] <- (rasterobject[[i]] - minval) * fact
     if (truncate)
       rasterobject[[i]] <- round(rasterobject[[i]], 0)
-    cat(sprintf("\n%s: Scale band %0*d of %d", pizzR::Systime(), n.bands.chars, i, n.bands))
+    gc(reset = T, full = T)
   }
   terra::setMinMax(rasterobject, force = T)
   cat(sprintf("\n\n%s: Done ...\n\n", pizzR::Systime()))
