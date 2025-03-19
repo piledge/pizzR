9526cf7652a1346da1898e28a748ce40d7b0d558
piledge <michael.lechner@icloud.com>
2025-03-19 13:54:27 +0100

    aws check

--- R/aws_S2_download_tile.R
+++ R/aws_S2_download_tile.R
@@ -1,26 +1,28 @@
 aws_S2_download_tile <- function(tile, year, month, day, cloud_cover, d_path='C:/temp/'){
-  
+  aws_cli <- Sys.which("aws")
+  if (aws_cli == "") stop("AWS-CLI not found. Please install it from 'https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html'")
+
   tile_1 <- substr(tile, 1, 2)
   tile_2 <- substr(tile, 3, 3)
   tile_3 <- substr(tile, 4, 5)
-  
+
   tile_folder <- sprintf('%04d-%02d-%02d', year, month, day)
 
   local_folder <- file.path(d_path, tile, year, tile_folder)
   if(!dir.exists(local_folder)) dir.create(local_folder, recursive = TRUE)
-  
-  aws_ls <- sprintf('aws s3 ls s3://sentinel-s2-l2a/tiles/%s/%s/%s/%s/%s/%s/ --no-sign-request', 
+
+  aws_ls <- sprintf('aws s3 ls s3://sentinel-s2-l2a/tiles/%s/%s/%s/%s/%s/%s/ --no-sign-request',
                     tile_1, tile_2, tile_3, year, month, day)
   tile_exists <- suppressWarnings(system(aws_ls, intern = TRUE))
   s_trim <- trimws(tile_exists)
   folder_id <- sub(".*PRE ([0123456789])/?$", "\\1", s_trim)
   folder_id <- folder_id[length(folder_id)]
-  
+
   metadata_aws_cp <- sprintf('aws s3 cp s3://sentinel-s2-l2a/tiles/%s/%s/%s/%d/%s/%s/%s/metadata.xml %s/metadata.xml --no-sign-request',
                              tile_1, tile_2, tile_3, year, month, day, folder_id, local_folder)
   aws_cp <- sprintf('aws s3 cp "s3://sentinel-s2-l2a/tiles/%s/%s/%s/%d/%d/%d/%s/" "%s" --recursive --no-sign-request',
                     tile_1, tile_2, tile_3, year, month, day, folder_id, local_folder)
-  
+
   cat(sprintf('\nStarting to Download tile %s ...\n', tile_folder))
   system(metadata_aws_cp)
   doc <- xml2::read_xml(file.path(local_folder, 'metadata.xml'))
