7d5385440887d93f6f9221a584588e8f661e22dd
piledge <michael.lechner@icloud.com>
2025-04-09 15:26:01 +0200

    refactor

--- R/species_percentage.R
+++ R/species_percentage.R
@@ -1,24 +1,23 @@
-species_percentage <- function(rst, shp, classnames = NULL, digits = NULL) {
-
+species_percentage <- function (rst, shp, classes='class', classnames = NULL, digits = NULL){
   extr <- terra::extract(rst, shp)
   extr <- extr[complete.cases(extr), ]
 
+  class_col <- which(classes == colnames(extr))
   unique_polys <- unique(extr$ID)
   n_polys <- length(unique_polys)
-  n_classes <- length(unique(extr$NPDA_S2_PS_map_masked))
+  n_classes <- length(unique(extr[, class_col]))
 
   extr$ID <- factor(extr$ID, levels = unique_polys)
-  extr$NPDA_S2_PS_map_masked <- factor(extr$NPDA_S2_PS_map_masked, levels = 1:n_classes)
+  extr[, class_col] <- factor(extr[, class_col], levels = 1:n_classes)
 
-  tbl <- table(extr$ID, extr$NPDA_S2_PS_map_masked)
-
+  tbl <- table(extr$ID, extr[, class_col])
   percent_tbl <- sweep(tbl, 1, rowSums(tbl), FUN = "/") * 100
-
   res <- as.data.frame.matrix(percent_tbl)
   colnames(res) <- paste0("class_", 1:n_classes)
 
-  if (!is.null(digits))     res <- round(res, digits)
-  if (!is.null(classnames)) colnames(res) <- classnames
-
+  if (!is.null(digits))
+    res <- round(res, digits)
+  if (!is.null(classnames))
+    colnames(res) <- classnames
   return(res)
 }
