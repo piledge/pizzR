2c147da118bf3ca2e90ff4a8be51f656f6570167
piledge <70260300+piledge@users.noreply.github.com>
2025-04-15 09:36:09 +0200

    Update corr_red.R

--- R/corr_red.R
+++ R/corr_red.R
@@ -1,15 +1,16 @@
-corr_red <- function (data, classes = 'class', cutoff = 0.8){
+corr_red <- function(data, classes = 'class', cutoff = 0.8) {
   pizzR::package.install('caret')
+  
   classes_col <- which(colnames(data) %in% classes)
   id_col <- which(colnames(data) %in% 'ID')
-
-  stopifnot(cutoff >= 0, cutoff <= 1, length(classes_col) <= 1, id_col <= 1)
-
-  cor_data <- data[-c(id_col, classes_col)]
+  stopifnot(cutoff >= 0, cutoff <= 1, length(classes_col) <= 1, length(id_col) <= 1)
+  
+  cor_data <- data[, -c(id_col, classes_col), drop = FALSE]
   cor_matrix <- cor(cor_data)
+  
   high_corr_idx <- caret::findCorrelation(cor_matrix, cutoff = cutoff)
-  low_cor <- cor_data[-high_corr_idx]
-
-  small_corr <- cbind(data[id_col], data[classes_col], cor_data[-high_corr])
+  low_cor <- if (length(high_corr_idx) > 0) cor_data[, -high_corr_idx, drop = FALSE] else cor_data
+  
+  small_corr <- cbind(data[, id_col, drop = FALSE], data[, classes_col, drop = FALSE], low_cor)
   return(small_corr)
 }
