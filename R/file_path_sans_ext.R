e9c2f654323f956f4df34cdad38c97c71ab14f37
MrFlywheel <n.mihatsch@gmail.com>
2025-04-16 11:08:42 +0200

    File paths with length < 2 included still file-extension. Fixed this to also work for paths with length = 1

--- R/file_path_sans_ext.R
+++ R/file_path_sans_ext.R
@@ -1,5 +1,5 @@
 file_path_sans_ext <- function(x,compression=FALSE) {
   if (compression)
     x <- sub("[.](gz|bz2|xz)$", "", x)
-  sub("([^.]+.+)\\.[[:alnum:]]+$", "\\1", x)
+  sub("(.+)\\.[[:alnum:]]+$", "\\1", x)
 }
