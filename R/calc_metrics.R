d1e747fcf0a28ee7a9b486357ff5af23db455385
piledge <70260300+piledge@users.noreply.github.com>
2025-04-22 09:39:07 +0200

    Update calc_metrics.R
    fixed edgecase of single-feature-data; added ID-column to output

--- R/calc_metrics.R
+++ R/calc_metrics.R
@@ -10,14 +10,14 @@ calc_metrics <- function (data, id = "ID", verbose = FALSE)
                     "080q", "085q", "090q", "095q", "iqr", "mean", "sd")
   metrics_names_rep <- paste(rep(colnames(data)[-id_col], length(metric_names)),
                              rep(metric_names, each = ncol(data) - 1), sep = "_")
-
+  
   iqr_apply <- function(x)  return(matrix(unlist(lapply(x, IQR))))
-
+  
   res <- data.frame((matrix(NA, nrow = n_samples, ncol = length(metrics_names_rep))))
   colnames(res) <- metrics_names_rep
   for (i in seq_along(unique_ids)) {
     if (verbose) pizzR::loop_progress(i, total_digits = nchar_n_samples)
-    data_subs <- na.omit(data[data$ID == i, -id_col])
+    data_subs <- na.omit(data[ data$ID == i, -id_col, drop = FALSE])
     metrics <- c(pizzR::quapply(data_subs, 0.05),
                  pizzR::quapply(data_subs, 0.10),
                  pizzR::quapply(data_subs, 0.15),
@@ -40,9 +40,10 @@ calc_metrics <- function (data, id = "ID", verbose = FALSE)
                  iqr_apply(data_subs),
                  colMeans(data_subs),
                  apply(data_subs, 2, sd))
-
+    
     res[i, ] <- c(metrics)
   }
+  res <- data.frame(ID=unique_ids, res)
   gc(reset = T, full = T)
   return(res)
 }
