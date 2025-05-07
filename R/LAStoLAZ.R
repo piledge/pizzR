d141d567641a90985b38624fd8f68244d13ebe7a
piledge <michael.lechner@icloud.com>
2025-05-07 11:45:34 +0200

    bugfix

--- R/LAStoLAZ.R
+++ R/LAStoLAZ.R
@@ -1,21 +1,21 @@
-LAStoLAZ <- function(x, y = NULL, verbose = TRUE){
+LAStoLAZ <- function(in_path, out_path = NULL, verbose = TRUE)
+{
+  pizzR::package.install("lidR", verbose = 1)
 
-  pizzR::package.install('lidR', verbose = 1)
+  if (is.null(out_path)) out_path <- file.path(in_path, "output_LAStoLAZ")
+  if (!dir.exists(out_path)) pizzR::setcreate.wd(out_path, verbose = F)
+  stopifnot(is.logical(verbose), dir.exists(in_path))
 
-  if (is.null(y)) y <- file.path(dirname(x), 'output_LAStoLAZ')
-  if (!dir.exists(y)) pizzR::setcreate.wd(y)
-  stopifnot(is.logical(verbose), dir.exists(x))
-  
-  lasfiles <- list.files(x, pattern = '\\.las$', full.names = TRUE)
+  lasfiles <- list.files(in_path, pattern = '\\.las$', full.names = TRUE)
   nfiles <- length(lasfiles)
-  stopifnot(nfiles > 0, msg = 'Input-folder empty!')
+  stopifnot(nfiles > 0)
 
-  if (verbose) cat(sprintf('\nWriting LAZ-files to "%s"\n\n', y))
+  if (verbose) cat(sprintf("\nWriting LAZ-files to '%s'\n\n", out_path))
   for (i in seq_along(lasfiles)) {
-    if (verbose) cat(sprintf("Compressing: %d / %d   \r", i, nfiles))
+    if (verbose) cat(sprintf('Compressing: %d / %d   \r', i, nfiles))
 
     las <- lidR::readLAS(lasfiles[i])
-    output_file <- file.path(y, sub('\\.las$', '.laz', basename(lasfiles[i])))
+    output_file <- file.path(out_path, sub('\\.las$', '.laz', basename(lasfiles[i])))
     lidR::writeLAS(las, output_file)
   }
   if (verbose) cat('\nCompression complete!\n')
